1429b0f82209b3c98a8562fbf82dc29cc06ce931
Jakub Skokan <jakub.skokan@havefun.cz>
2016-11-21 18:31:33 +0100

    vpsadmin-config: Add api.rb to send exceptions via email

---
+++ configs/vpsadmin/api/api.rb
@@ -0,0 +1,8 @@
+api do |server|
+  server.extensions << HaveAPI::Extensions::ExceptionMailer.new(
+      from: 'vpsadmin@vpsadmin.vpsfree.cz',
+      to: 'aither@havefun.cz',
+      subject: '[vpsAdmin API Error] %s',
+      smtp: false,
+  )
+end
