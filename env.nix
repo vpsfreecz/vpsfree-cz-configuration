ba099924866e8def1d08516d3bdaa2da0fbee3cf
Richard Marko <srk@48.io>
2018-02-20 17:57:53 +0100

    move hydra specifics from env to hydra-common

--- env.nix
+++ env.nix
@@ -4,9 +4,6 @@
   networking.nameservers = [ "172.18.2.10" "172.18.2.11" "208.67.222.222" "208.67.220.220" ];
   services.openssh.enable = true;
   nix.useSandbox = true;
-  nix.nrBuildUsers = 100;
-  nix.buildCores = 0;
-  systemd.tmpfiles.rules = [ "d /tmp 1777 root root 7d" ];
 
   environment.systemPackages = with pkgs; [
     wget
--- hydra-common.nix
+++ hydra-common.nix
@@ -6,6 +6,10 @@
   services.openssh.allowSFTP = false;
   services.openssh.passwordAuthentication = false;
 
+  nix.nrBuildUsers = 100;
+  nix.buildCores = 0;
+  systemd.tmpfiles.rules = [ "d /tmp 1777 root root 7d" ];
+
   users = {
     mutableUsers = false;
     users.root.openssh.authorizedKeys.keyFiles = [ ~/.ssh/id_rsa.pub ];
