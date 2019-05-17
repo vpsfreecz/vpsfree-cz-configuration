58e9d453b676c50e52b2df370444885d60af7173
Richard Marko <srk@48.io>
2019-05-17 14:21:49 +0200

    env: make nameservers default

--- env.nix
+++ env.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   time.timeZone = "Europe/Amsterdam";
-  networking.nameservers = [ "172.18.2.10" "172.18.2.11" "208.67.222.222" "208.67.220.220" ];
+  networking.nameservers = pkgs.lib.mkDefault [ "172.18.2.10" "172.18.2.11" "208.67.222.222" "208.67.220.220" ];
   services.openssh.enable = true;
   nix.useSandbox = true;
 
