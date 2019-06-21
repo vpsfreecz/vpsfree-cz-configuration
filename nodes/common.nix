67711785eccae4a4e3782024cddf776da44b3a53
Richard Marko <srk@48.io>
2019-06-21 16:31:12 +0200

    nodes/common: be explicit about copying paths to store when adding ssh host keys

--- nodes/common.nix
+++ nodes/common.nix
@@ -101,11 +101,12 @@
 
   environment.etc =
   let prefix = "/secrets/nodes/${ config.networking.hostName }/ssh";
+      path = pkgs.copyPathToStore prefix;
   in
   {
-    "ssh/ssh_host_rsa_key.pub".source = "${ prefix }/ssh_host_rsa_key.pub";
-    "ssh/ssh_host_rsa_key" = { mode = "0600"; source = "${ prefix }/ssh_host_rsa_key"; };
-    "ssh/ssh_host_ed25519_key.pub".source = "${ prefix }/ssh_host_ed25519_key.pub";
-    "ssh/ssh_host_ed25519_key" = { mode = "0600"; source = "${ prefix}/ssh_host_ed25519_key"; };
+    "ssh/ssh_host_rsa_key.pub".source = "${ path }/ssh_host_rsa_key.pub";
+    "ssh/ssh_host_rsa_key" = { mode = "0600"; source = "${ path }/ssh_host_rsa_key"; };
+    "ssh/ssh_host_ed25519_key.pub".source = "${ path }/ssh_host_ed25519_key.pub";
+    "ssh/ssh_host_ed25519_key" = { mode = "0600"; source = "${ path }/ssh_host_ed25519_key"; };
   };
 }
