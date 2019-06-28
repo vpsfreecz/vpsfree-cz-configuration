ae67a13057ec730e1f6e6ff516f89fd8cc1da1c6
Richard Marko <srk@48.io>
2019-06-28 11:27:34 +0200

    modules/deploy: fix script vars escaping

--- modules/deploy.nix
+++ modules/deploy.nix
@@ -44,10 +44,10 @@ in
     (pkgs.writeScriptBin "generate-node-sshkeys" ''
         set -e
         test $# -eq 1 || { echo "Expects node hostname"; exit 1; }
-        test -d /secrets/nodes/"${1}" && { echo "Already there"; exit 1; }
-        mkdir -p /secrets/nodes/"${1}"/ssh
-        ssh-keygen -t rsa -b 4096 -f /secrets/nodes/"${1}"/ssh/ssh_host_rsa_key -N ""
-        ssh-keygen -t ed25519 -f /secrets/nodes/"${1}"/ssh/ssh_host_ed25519_key -N ""
+        test -d /secrets/nodes/"''${1}" && { echo "Already there"; exit 1; }
+        mkdir -p /secrets/nodes/"''${1}"/ssh
+        ssh-keygen -t rsa -b 4096 -f /secrets/nodes/"''${1}"/ssh/ssh_host_rsa_key -N ""
+        ssh-keygen -t ed25519 -f /secrets/nodes/"''${1}"/ssh/ssh_host_ed25519_key -N ""
       '')
 
   ];
