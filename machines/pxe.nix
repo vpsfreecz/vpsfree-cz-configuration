b7c9b05116e268de296367c2a8d88e9b4dac631c
Richard Marko <srk@48.io>
2019-06-28 19:28:37 +0200

    machines/pxe: allow HTTP from DHCP subnets only

--- machines/pxe.nix
+++ machines/pxe.nix
@@ -12,5 +12,9 @@ in
     host = "172.16.254.5";
     inherit (images) nixosItems vpsadminosItems mappings;
     includeNetbootxyz = true;
+    allowedIPRanges = [
+      "172.16.254.0/24"
+      "172.19.254.0/24"
+    ];
   };
 }
