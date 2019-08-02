0e374cf66dc152c3816d0977650bac715a9aaecd
Jakub Skokan <jakub.skokan@havefun.cz>
2019-08-02 22:33:03 +0200

    Add bgp networks among management networks
    This will enable them in firewall and sharenfs.

--- data/networks/management.nix
+++ data/networks/management.nix
@@ -3,5 +3,15 @@
     { location = "prg"; address = "172.16.0.0"; prefix = 23; }
     { location = "brq"; address = "172.19.0.0"; prefix = 23; }
     { location = "pgnd"; address = "172.16.2.0"; prefix = 23; }
+
+    # bgp prg
+    { location = "prg"; address = "172.16.253.0"; prefix = 24; }
+    { location = "prg"; address = "172.16.252.0"; prefix = 24; }
+    { location = "prg"; address = "172.16.251.0"; prefix = 24; }
+    { location = "prg"; address = "172.16.250.0"; prefix = 24; }
+
+    # bgp brq
+    { location = "brq"; address = "172.19.253.0"; prefix = 24; }
+    { location = "brq"; address = "172.19.252.0"; prefix = 24; }
   ];
 }
