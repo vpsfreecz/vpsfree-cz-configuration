d4cae6b92ab95886bc2d09decfd53fe598bb9292
Jakub Skokan <jakub.skokan@havefun.cz>
2019-08-02 20:22:27 +0200

    nodes: restrict udev netif rename rules to ethernet devices

--- nodes/lib.nix
+++ nodes/lib.nix
@@ -4,7 +4,7 @@ with lib;
 
 rec {
   mkNetUdevRule = name: mac: ''
-  ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="${mac}", NAME="${name}"
+  ACTION=="add", SUBSYSTEM=="net", DRIVERS=="?*", KERNEL=="eth*", ATTR{address}=="${mac}", NAME="${name}"
   '';
 
   mkNetUdevRules = rs: concatStringsSep "\n" (mapAttrsToList (name: mac: mkNetUdevRule name mac) rs);
