7a688bfd26687124a579b5af0c15eaa84f448d4d
Jakub Skokan <jakub.skokan@havefun.cz>
2019-08-02 09:46:30 +0200

    mon0: introduce alerting

--- containers/vpsfree.cz/mon0.nix
+++ containers/vpsfree.cz/mon0.nix
@@ -10,6 +10,7 @@
     firewall.allowedTCPPorts = [
       3000  # grafana
       9090  # prometheus
+      9093  # alertmanager
     ];
   };
 
@@ -66,7 +67,9 @@
                 "backuper.prg.vpsfree.cz:9100"
               ];
               labels = {
-                alias = "backuper";
+                alias = "backuper.prg";
+                location = "prg";
+                role = "storage";
               };
             }
             {
@@ -75,6 +78,8 @@
               ];
               labels = {
                 alias = "node1.stg";
+                location = "stg";
+                role = "hypervisor";
               };
             }
             {
@@ -83,6 +88,8 @@
               ];
               labels = {
                 alias = "node2.stg";
+                location = "stg";
+                role = "hypervisor";
               };
             }
           ];
@@ -101,21 +108,118 @@
             }
             {
               targets = [
-                "vpsadminos.org:9100"
+                "www.vpsadminos.org:9100"
               ];
               labels = {
-                alias = "vpsadminos.org";
+                alias = "www.vpsadminos.org";
               };
             }
           ];
         }
       ];
+
+      alertmanagerURL = [
+        "localhost:${toString config.services.prometheus.alertmanager.port}"
+      ];
+
+      rules = [
+        ''
+          groups:
+          - name: main
+            rules:
+            - alert: ExporterDown
+              expr: up == 0
+              for: 3m
+              labels:
+                severity: warning
+              annotations:
+                summary: "Exporter down (instance {{ $labels.instance }})"
+                description: "Prometheus exporter down\n  VALUE = {{ $value }}\n  LABELS: {{ $labels }}"
+
+            - alert: HypervisorHighCpuLoad
+              expr: 100 - (avg by(instance) (irate(node_cpu_seconds_total{mode="idle",role="hypervisor"}[5m])) * 100) > 80
+              for: 5m
+              labels:
+                severity: warning
+              annotations:
+                summary: "High CPU load (instance {{ $labels.instance }})"
+                description: "CPU load is > 80%\n  VALUE = {{ $value }}\n  LABELS: {{ $labels }}"
+
+            - alert: HypervisorHighIoWait
+              expr: (avg by(instance) (irate(node_cpu_seconds_total{mode="iowait",role="hypervisor"}[5m])) * 100) > 20
+              for: 5m
+              labels:
+                severity: warning
+              annotations:
+                summary: "High CPU iowait (instance {{ $labels.instance }})"
+                description: "CPU iowait is > 20%\n  VALUE = {{ $value }}\n  LABELS: {{ $labels }}"
+
+            - alert: StorageHighCpuLoad
+              expr: 100 - (avg by(instance) (irate(node_cpu_seconds_total{mode="idle",role="storage"}[5m])) * 100) > 80
+              for: 15m
+              labels:
+                severity: warning
+              annotations:
+                summary: "High CPU load (instance {{ $labels.instance }})"
+                description: "CPU load is > 80%\n  VALUE = {{ $value }}\n  LABELS: {{ $labels }}"
+
+            - alert: StorageHighIoWait
+              expr: (avg by(instance) (irate(node_cpu_seconds_total{mode="iowait",role="storage"}[5m])) * 100) > 30
+              for: 15m
+              labels:
+                severity: warning
+              annotations:
+                summary: "High CPU iowait (instance {{ $labels.instance }})"
+                description: "CPU iowait is > 30%\n  VALUE = {{ $value }}\n  LABELS: {{ $labels }}"
+
+            - alert: LowZfsArcC
+              expr: node_zfs_arc_c < (node_memory_MemTotal_bytes / 8)
+              for: 5m
+              labels:
+                severity: warning
+              annotations:
+                summary: "ZFS arc_c too low (instance {{ $labels.instance }})"
+                description: "ZFS arc_c is too low (less than 1/8 of total memory)\n  VALUE = {{ $value }}\n  LABELS: {{ $labels }}"
+        ''
+      ];
     };
+
+    prometheus.alertmanager = {
+      enable = true;
+      configuration = {
+        "global" = {
+          "smtp_smarthost" = "localhost:25";
+          "smtp_from" = "alertmanager@vpsfree.cz";
+          "smtp_require_tls" = false;
+        };
+        "route" = {
+          "group_by" = [ "alertname" "alias" ];
+          "group_wait" = "30s";
+          "group_interval" = "2m";
+          "repeat_interval" = "4h";
+          "receiver" = "team-admins";
+        };
+        "receivers" = [
+          {
+            "name" = "team-admins";
+            "email_configs" = [
+              {
+                "to" = "aither@havefun.cz";
+                "send_resolved" = true;
+              }
+            ];
+          }
+        ];
+      };
+    };
+
     grafana = {
       enable = true;
       addr = "0.0.0.0";
       domain = "grafana.vpsfree.cz";
       rootUrl = "http://grafana.vpsfree.cz/";
     };
+
+    postfix.enable = true;
   };
 }
