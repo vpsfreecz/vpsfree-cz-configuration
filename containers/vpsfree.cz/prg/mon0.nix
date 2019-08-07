2647f047cb6e96fe3c2be57c9df8429d8788d46e
Jakub Skokan <jakub.skokan@havefun.cz>
2019-08-07 18:41:48 +0200

    mon0.prg: add alerts for zpool checks

--- containers/vpsfree.cz/prg/mon0.nix
+++ containers/vpsfree.cz/prg/mon0.nix
@@ -124,6 +124,53 @@
       rules = [
         ''
           groups:
+          - name: common
+            rules:
+            - alert: ZpoolListFailed
+              expr: zpool_list_success != 0
+              for: 5m
+              labels:
+                severity: critical
+              annotations:
+                summary: "zpool list failed (instance {{ $labels.instance }})"
+                description: "An error occurred while running zpool list\n  LABELS: {{ $labels }}"
+
+            - alert: ZpoolListParseError
+              expr: zpool_list_parse_success != 0
+              labels:
+                severity: warning
+              annotations:
+                summary: "Unexpected zpool list output (instance {{ $labels.instance }})"
+                description: "An error occurred while parsing output of zpool list\n  LABELS: {{ $labels }}"
+
+            - alert: DegradedZpool
+              expr: zpool_list_healt != 0
+              labels:
+                severity: critical
+                frequency: daily
+              annotations:
+                summary: "Zpool is degraded (instance {{ $labels.instance }})"
+                description: "One or more devices have failed\n  LABELS: {{ $labels }}"
+
+            - alert: ZpoolLowFreeSpace
+              expr: zpool_list_capacity >= 80
+              for: 1h
+              labels:
+                severity: warning
+              annotations:
+                summary: "Not enough free space (instance {{ $labels.instance }})"
+                description: "Zpool uses more than 80% of available space\n  VALUE = {{ $value }}\n  LABELS: {{ $labels }}"
+
+            - alert: ZpoolCritFreeSpace
+              expr: zpool_list_capacity >= 90
+              for: 15m
+              labels:
+                severity: critical
+                frequency: daily
+              annotations:
+                summary: "Not enough free space (instance {{ $labels.instance }})"
+                description: "Zpool uses more than 90% of available space\n  VALUE = {{ $value }}\n  LABELS: {{ $labels }}"
+
           - name: nodes
             interval: 20s
             rules:
