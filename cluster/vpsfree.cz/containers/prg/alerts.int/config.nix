9b0b2dbc7754a390bb0ed0627ba2dfe73848dbd8
Jakub Skokan <jakub.skokan@havefun.cz>
2019-08-14 13:50:44 +0200

    alerts.int.prg: remove unnecessary quotes

--- cluster/vpsfree.cz/containers/prg/alerts.int/config.nix
+++ cluster/vpsfree.cz/containers/prg/alerts.int/config.nix
@@ -47,131 +47,131 @@ in {
     port = alertmanagerPort;
     webExternalUrl = "https://alerts.prg.vpsfree.cz/";
     configuration = {
-      "global" = {
-        "smtp_smarthost" = "localhost:25";
-        "smtp_from" = "alertmanager@vpsfree.cz";
-        "smtp_require_tls" = false;
+      global = {
+        smtp_smarthost = "localhost:25";
+        smtp_from = "alertmanager@vpsfree.cz";
+        smtp_require_tls = false;
       };
-      "route" = {
-        "group_by" = [ "alertname" "alias" ];
-        "group_wait" = "30s";
-        "group_interval" = "2m";
-        "repeat_interval" = "4h";
-        "receiver" = "team-mail";
+      route = {
+        group_by = [ "alertname" "alias" ];
+        group_wait = "30s";
+        group_interval = "2m";
+        repeat_interval = "4h";
+        receiver = "team-mail";
 
-        "routes" = [
+        routes = [
           # Mail alerts
           {
-            "match" = {
-              "severity" = "warning";
+            match = {
+              severity = "warning";
             };
-            "group_wait" = "30s";
-            "group_interval" = "2m";
-            "repeat_interval" = "4h";
-            "receiver" = "team-mail";
-            "continue" = false;
+            group_wait = "30s";
+            group_interval = "2m";
+            repeat_interval = "4h";
+            receiver = "team-mail";
+            continue = false;
 
-            "routes" = [
+            routes = [
               {
-                "match" = {
-                  "frequency" = "daily";
+                match = {
+                  frequency = "daily";
                 };
-                "repeat_interval" = "24h";
+                repeat_interval = "24h";
               }
               {
-                "match" = {
-                  "frequency" = "hourly";
+                match = {
+                  frequency = "hourly";
                 };
-                "repeat_interval" = "1h";
+                repeat_interval = "1h";
               }
             ];
           }
 
           # SMS alerts
           {
-            "match" = {
-              "severity" = "critical";
+            match = {
+              severity = "critical";
             };
-            "receiver" = "team-sms";
-            "group_wait" = "10s";
-            "repeat_interval" = "10m";
-            "continue" = false;
+            receiver = "team-sms";
+            group_wait = "10s";
+            repeat_interval = "10m";
+            continue = false;
 
-            "routes" = [
+            routes = [
               {
-                "match" = {
-                  "frequency" = "daily";
+                match = {
+                  frequency = "daily";
                 };
-                "repeat_interval" = "24h";
+                repeat_interval = "24h";
               }
               {
-                "match" = {
-                  "frequency" = "6h";
+                match = {
+                  frequency = "6h";
                 };
-                "repeat_interval" = "6h";
+                repeat_interval = "6h";
               }
               {
-                "match" = {
-                  "frequency" = "hourly";
+                match = {
+                  frequency = "hourly";
                 };
-                "repeat_interval" = "1h";
+                repeat_interval = "1h";
               }
               {
-                "match" = {
-                  "frequency" = "15m";
+                match = {
+                  frequency = "15m";
                 };
-                "repeat_interval" = "15m";
+                repeat_interval = "15m";
               }
               {
-                "match" = {
-                  "frequency" = "10m";
+                match = {
+                  frequency = "10m";
                 };
-                "repeat_interval" = "10m";
+                repeat_interval = "10m";
               }
               {
-                "match" = {
-                  "frequency" = "5m";
+                match = {
+                  frequency = "5m";
                 };
-                "repeat_interval" = "5m";
+                repeat_interval = "5m";
               }
               {
-                "match" = {
-                  "frequency" = "2m";
+                match = {
+                  frequency = "2m";
                 };
-                "repeat_interval" = "2m";
+                repeat_interval = "2m";
               }
               {
-                "match_re" = {
-                  "frequency" = "1m|minutely";
+                match_re = {
+                  frequency = "1m|minutely";
                 };
-                "repeat_interval" = "1m";
+                repeat_interval = "1m";
               }
               {
-                "match" = {
-                  "frequency" = "15s";
+                match = {
+                  frequency = "15s";
                 };
-                "repeat_interval" = "15s";
+                repeat_interval = "15s";
               }
             ];
           }
         ];
       };
-      "receivers" = [
+      receivers = [
         {
-          "name" = "team-mail";
-          "email_configs" = [
+          name = "team-mail";
+          email_configs = [
             {
-              "to" = "aither@havefun.cz,snajpa@snajpa.net";
-              "send_resolved" = true;
+              to = "aither@havefun.cz,snajpa@snajpa.net";
+              send_resolved = true;
             }
           ];
         }
         {
-          "name" = "team-sms";
-          "webhook_configs" = [
+          name = "team-sms";
+          webhook_configs = [
             {
-              "url" = "http://localhost:9876/alert";
-              "send_resolved" = true;
+              url = "http://localhost:9876/alert";
+              send_resolved = true;
             }
           ];
         }
