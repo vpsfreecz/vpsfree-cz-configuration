b5171e531c39fcf2e601cbb611ead34b13e58525
Jakub Skokan <jakub.skokan@havefun.cz>
2019-09-18 17:22:33 +0200

    cluster: add initial config for nasbox.prg

--- cluster/vpsfree.cz/module-list.nix
+++ cluster/vpsfree.cz/module-list.nix
@@ -9,6 +9,8 @@
   ./nodes/brq/openvz.nix
   ./nodes/pgnd/openvz.nix
   ./nodes/prg/backuper/module.nix
+  # Remove nasbox from ./nodes/prg/openvz.nix when enabling
+  # ./nodes/prg/nasbox/module.nix
   ./nodes/prg/openvz.nix
   ./nodes/stg/node1/module.nix
   ./nodes/stg/node2/module.nix
