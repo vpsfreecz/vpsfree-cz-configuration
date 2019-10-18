fd589c1989927926f1ee9dba624050e4ae6379a4
Jakub Skokan <jakub.skokan@havefun.cz>
2019-10-18 18:11:33 +0200

    nasbox.prg: remove bonding, configure bgp

--- cluster/cz.vpsfree/nodes/prg/nasbox/module.nix
+++ cluster/cz.vpsfree/nodes/prg/nasbox/module.nix
@@ -1,5 +1,17 @@
 { config, ... }:
-{
+let
+  allAddresses = {
+    primary = { address = "172.16.0.6"; prefix = 32; };
+    teng0 = {
+      v4 = [ { address = "172.16.251.26"; prefix = 30; } ];
+      v6 = [ { address = "2a03:3b40:42:2:07::2"; prefix = 80; } ];
+    };
+    teng1 = {
+      v4 = [ { address = "172.16.250.26"; prefix = 30; } ];
+      v6 = [ { address = "2a03:3b40:42:3:07::2"; prefix = 80; } ];
+    };
+  };
+in {
   cluster."cz.vpsfree".prg.nasbox = rec {
     type = "node";
     spin = "vpsadminos";
@@ -19,7 +31,11 @@
       ];
     };
 
-    addresses.primary = { address = "172.16.0.6"; prefix = 23; };
+    addresses = with allAddresses; {
+      inherit primary;
+      v4 = teng0.v4 ++ teng1.v4;
+      v6 = teng0.v6 ++ teng1.v6;
+    };
 
     osNode = {
       networking = {
@@ -32,13 +48,27 @@
             teng0 = "00:25:90:0e:5a:66";
             teng1 = "00:25:90:0e:5a:67";
           };
+          addresses = {
+            inherit (allAddresses) teng0 teng1;
+          };
         };
 
         bird = {
-          enable = false; # don't need bird as long as bonding is in use
+          as = 4200001007;
+          routerId = "172.16.0.6";
+          bgpNeighbours = {
+            v4 = [
+              { address = "172.16.251.25"; as = 4200001901; }
+              { address = "172.16.250.25"; as = 4200001902; }
+            ];
+            v6 = [
+              { address = "2a03:3b40:42:2:07::1"; as = 4200001901; }
+              { address = "2a03:3b40:42:3:07::1"; as = 4200001902; }
+            ];
+          };
         };
 
-        virtIP = null;
+        virtIP = addresses.primary;
       };
     };
 
