dae6bdb48187d7eb77f079bbab6ca6429e773099
Jakub Skokan <jakub.skokan@havefun.cz>
2020-03-30 22:36:55 +0200

    node2.stg: disable shaper by masking tc from nodectld

--- cluster/cz.vpsfree/nodes/stg/node2/config.nix
+++ cluster/cz.vpsfree/nodes/stg/node2/config.nix
@@ -1,4 +1,4 @@
-{ config, lib, ...}:
+{ config, pkgs, lib, ...}:
 {
   imports = [
     ../common.nix
@@ -27,4 +27,23 @@
       };
     };
   };
+
+  # Temporarily disable tc which is used to configure shaper
+  runit.services.nodectld.run =
+    let
+      bindir = pkgs.runCommand "bindir" {} ''
+        mkdir -p $out/bin
+        cat <<EOF > $out/bin/tc
+        #!/bin/sh
+        exit 0
+        EOF
+        chmod +x $out/bin/tc
+      '';
+    in lib.mkForce ''
+      ulimit -c unlimited
+      export PATH="${bindir}/bin:$PATH"
+      export HOME=${config.users.extraUsers.root.home}
+      exec 2>&1
+      exec ${pkgs.nodectld}/bin/nodectld --log syslog --log-facility local3 --export-console
+    '';
 }
