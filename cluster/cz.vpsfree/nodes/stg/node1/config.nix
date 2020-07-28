a2f29396ffb4b8303f5b970f41955f6ece2dd9f2
Jakub Skokan <jakub.skokan@havefun.cz>
2020-07-28 17:46:39 +0200

    node1.stg: set ashift. use only nvmes

--- cluster/cz.vpsfree/nodes/stg/node1/config.nix
+++ cluster/cz.vpsfree/nodes/stg/node1/config.nix
@@ -9,23 +9,12 @@
   boot.zfs.pools = {
     tank = {
       install = true;
-      wipe = [ "sda" "sdb" "nvme0n1" "nvme1n1" "nvme2n1" "nvme3n1" ];
+      wipe = [ "nvme0n1" "nvme1n1" "nvme2n1" "nvme3n1" ];
       layout = [
         { type = "raidz"; devices = [ "nvme0n1" "nvme1n1" "nvme2n1" "nvme3n1" ]; }
       ];
-      log = [
-        { mirror = true; devices = [ "sda1" "sdb1" ]; }
-      ];
-      cache = [ "sda2" "sdb2" ];
-      partition = {
-        sda = {
-          p1 = { sizeGB=10; };
-          p2 = {};
-        };
-        sdb = {
-          p1 = { sizeGB=10; };
-          p2 = {};
-        };
+      properties = {
+        ashift = "12";
       };
     };
   };
