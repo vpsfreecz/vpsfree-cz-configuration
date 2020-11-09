a613cbf440ccdd6bdff9d608c7a2c17f0912d2fe
Jakub Skokan <jakub.skokan@havefun.cz>
2020-11-09 18:25:07 +0100

    build: do not use target = localhost yet
    confctl has to be fixed to remove ruby env variables before running the
    switch script locally.

--- cluster/cz.vpsfree/machines/build/module.nix
+++ cluster/cz.vpsfree/machines/build/module.nix
@@ -3,7 +3,7 @@
   cluster."cz.vpsfree/machines/build" = rec {
     spin = "vpsadminos";
     swpins.channels = [ "os-master" "nixos-unstable" ];
-    host = { name = "build"; domain = "vpsfree.cz"; target = "localhost"; };
+    host = { name = "build"; domain = "vpsfree.cz"; };
     addresses.primary = { address = "172.16.254.4"; prefix = 32; };
     services.node-exporter = {};
   };
