e3e4eb7a48a7103436bb8520a4b759573ec70afc
Jakub Skokan <jakub.skokan@havefun.cz>
2021-01-17 16:50:38 +0100

    log.prg: use older os compatible with nixos 19.03
    It should now be possible to upgrade log.prg to a newer nixos and remove
    this workaround. We've stayd on older nixos because of reworked/broken
    alerts in newer graylog, but as we don't even need alerts now, upgrade
    should be possible.

--- cluster/cz.vpsfree/containers/prg/log/module.nix
+++ cluster/cz.vpsfree/containers/prg/log/module.nix
@@ -2,7 +2,17 @@
 {
   cluster."cz.vpsfree/containers/prg/log" = rec {
     spin = "nixos";
-    swpins.channels = [ "nixos-19.03" "os-master" ];
+    swpins.channels = [ "nixos-19.03" ];
+    swpins.pins = {
+      vpsadminos = {
+        type = "git-rev";
+
+        git-rev = {
+          url = "https://github.com/vpsfreecz/vpsadminos";
+          update.ref = "41a176a346ff92ab14e6f46519947035ef9973d0";
+        };
+      };
+    };
     container.id = 14004;
     host = { name = "log"; location = "prg"; domain = "vpsfree.cz"; };
     addresses.primary = { address = "172.16.4.1"; prefix = 32; };
