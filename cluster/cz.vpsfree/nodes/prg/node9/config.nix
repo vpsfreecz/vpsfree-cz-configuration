a8eb832e086b2d33a740a472fcebdf1f27dcd49d
Jakub Skokan <jakub.skokan@havefun.cz>
2021-02-03 21:37:40 +0100

    node9.prg: update zpool disk names

--- cluster/cz.vpsfree/nodes/prg/node9/config.nix
+++ cluster/cz.vpsfree/nodes/prg/node9/config.nix
@@ -9,20 +9,20 @@
       install = true;
       wipe = [ "sda" "sdb" "sdc" "sdd" "sde" "sdf" "sdg" "sdh" "sdi" "sdj" ];
       layout = [
-        { type = "mirror"; devices = [ "sda" "sdb" ]; }
         { type = "mirror"; devices = [ "sdc" "sdd" ]; }
+        { type = "mirror"; devices = [ "sde" "sdf" ]; }
         { type = "mirror"; devices = [ "sdg" "sdh" ]; }
         { type = "mirror"; devices = [ "sdi" "sdj" ]; }
       ];
       log = [
-        { mirror = true; devices = [ "sde1" "sdf1" ]; }
+        { mirror = true; devices = [ "sda1" "sdb1" ]; }
       ];
       partition = {
-        sde = {
+        sda = {
           p1 = { sizeGB=10; };
           p2 = {};
         };
-        sdf = {
+        sdb = {
           p1 = { sizeGB=10; };
           p2 = {};
         };
