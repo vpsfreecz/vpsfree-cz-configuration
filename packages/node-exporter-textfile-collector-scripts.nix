21351fde26815e7a728dc8696776af597ff62561
Jakub Skokan <jakub.skokan@havefun.cz>
2021-04-19 17:54:00 +0200

    smartmon: update to track available_reservd_space and ssd_life_left

--- packages/node-exporter-textfile-collector-scripts.nix
+++ packages/node-exporter-textfile-collector-scripts.nix
@@ -1,6 +1,6 @@
 { stdenvNoCC, lib, fetchFromGitHub, bash, smartmontools }:
 let
-  rev = "80884d276984420c6f358c35b9140f246ecb0946";
+  rev = "fd6ed02e26c11995386d1f7a9db75025fae85de9";
   shortRev = builtins.substring 0 7 rev;
 in stdenvNoCC.mkDerivation rec {
   pname = "node-exporter-textfile-collector-scripts";
@@ -10,7 +10,7 @@ in stdenvNoCC.mkDerivation rec {
     owner = "vpsfreecz";
     repo = "node-exporter-textfile-collector-scripts";
     inherit rev;
-    sha256 = "sha256:0fn070g3hwclbjfclilcy0146nv0i92sxqaj5gc212nibk609r81";
+    sha256 = "sha256:1pr2sjfm01hwv1np6sshm9djnk8nwxzvrylan8ljmrfpgjyqpz2n";
   };
 
   buildInputs = [
