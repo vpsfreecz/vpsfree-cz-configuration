e7046dce6795aebfd2cc5f2931061df6fea32a51
Jakub Skokan <jakub.skokan@havefun.cz>
2021-05-17 09:38:42 +0200

    log.prg: allow access from all management networks

--- cluster/cz.vpsfree/containers/prg/log/config.nix
+++ cluster/cz.vpsfree/containers/prg/log/config.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, confLib, confMachine, ... }:
+{ config, pkgs, lib, confLib, confData, confMachine, ... }:
 with lib;
 let
   rsyslogTcpPort = confMachine.services.graylog-rsyslog-tcp.port;
@@ -19,6 +19,15 @@ in {
   networking.firewall = {
     allowedTCPPorts = [ 80 ];
     extraCommands = ''
+      ### Management networks
+      ${concatMapStringsSep "\n" (net: ''
+        # Allow access from ${net.location} @ ${net.address}/${toString net.prefix}
+        iptables -A nixos-fw -p tcp -s ${net.address}/${toString net.prefix} --dport ${toString rsyslogTcpPort} -j nixos-fw-accept
+        iptables -A nixos-fw -p udp -s ${net.address}/${toString net.prefix} --dport ${toString rsyslogUdpPort} -j nixos-fw-accept
+        iptables -A nixos-fw -p udp ${net.address}/${toString net.prefix} --dport ${toString gelfPort} -j nixos-fw-accept
+      '') confData.vpsadmin.networks.management.ipv4}
+
+      ### Individual machines
       ${concatMapStringsSep "\n" (a: ''
         # Allow access from ${a.config.host.fqdn} @ ${a.address}
         iptables -A nixos-fw -p tcp -s ${a.address} --dport ${toString rsyslogTcpPort} -j nixos-fw-accept
