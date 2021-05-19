6c9d2c756c8f5e995c9f0816ae99cbadb45518bb
Jakub Skokan <jakub.skokan@havefun.cz>
2021-05-19 16:56:53 +0200

    cluster: rename alerts.int to alerts1.int

--- cluster/cz.vpsfree/containers/prg/int.alerts1/module.nix
+++ cluster/cz.vpsfree/containers/prg/int.alerts1/module.nix
@@ -1,10 +1,10 @@
 { config, ... }:
 {
-  cluster."cz.vpsfree/containers/prg/int.alerts" = rec {
+  cluster."cz.vpsfree/containers/prg/int.alerts1" = rec {
     spin = "nixos";
     swpins.channels = [ "nixos-stable" "os-master" ];
     container.id = 14077;
-    host = { name = "alerts"; location = "int.prg"; domain = "vpsfree.cz"; };
+    host = { name = "alerts1"; location = "int.prg"; domain = "vpsfree.cz"; };
     addresses.primary = { address = "172.16.4.11"; prefix = 32; };
     services = {
       alertmanager = {};
