cb4f6e3cf8ecd98bbf67881fec626e432c244850
Jakub Skokan <jakub.skokan@havefun.cz>
2021-07-26 08:48:35 +0200

    build machine: switch to channel nixos-stable

--- cluster/cz.vpsfree/machines/build/module.nix
+++ cluster/cz.vpsfree/machines/build/module.nix
@@ -2,7 +2,7 @@
 {
   cluster."cz.vpsfree/machines/build" = rec {
     spin = "vpsadminos";
-    swpins.channels = [ "os-staging" "nixos-unstable" ];
+    swpins.channels = [ "os-staging" "nixos-stable" ];
     host = { name = "build"; domain = "vpsfree.cz"; };
     addresses.primary = { address = "172.16.254.4"; prefix = 32; };
     services.node-exporter = {};
