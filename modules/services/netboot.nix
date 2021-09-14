3cb7a191cae32163b91a848acb98a71968209275
Jakub Skokan <jakub.skokan@havefun.cz>
2021-09-14 20:00:30 +0200

    netboot: redesign to use pxelinux directly, remove iPXE

--- modules/services/netboot.nix
+++ modules/services/netboot.nix
@@ -1,206 +1,182 @@
 { lib, config, pkgs, ... }:
-
 with lib;
-
 let
   cfg = config.services.netboot;
   server = cfg.host;
   secretsDir = cfg.secretsDir;
-  # these are needed for iPXE build
-  # Warning: secretsDir will end up in a nix/store
-  caCert = "${secretsDir}/root.pem";
-  caCodesign = "${secretsDir}/codesign.crt";
 
-  ipxe_item_nixos = name: item: ''
-    :${name}
-    imgfree
-    imgfetch http://${server}/${name}/bzImage init=${builtins.unsafeDiscardStringContext item.toplevel}/init loglevel=7 || goto normal
-    imgfetch http://${server}/${name}/initrd || goto normal
-    imgverify bzImage http://${server}/${name}/bzImage.sig
-    imgverify initrd http://${server}/${name}/initrd.sig
-    imgselect bzImage
-    boot
-  '';
-
-  ipxe_boot_vpsadminos = { name, item, runlevel ? "default", kernelParams ? [] }: ''
-    imgfree
-    imgfetch http://${server}/${name}/kernel systemConfig=${builtins.unsafeDiscardStringContext item.toplevel} ${toString item.kernelParams} runlevel=${runlevel} ${concatStringsSep " " kernelParams} || goto normal
-    imgfetch http://${server}/${name}/initrd || goto normal
-    imgfetch http://${server}/${name}/root.squashfs root.squashfs || goto normal
-    imgverify kernel http://${server}/${name}/kernel.sig
-    imgverify initrd http://${server}/${name}/initrd.sig
-    imgverify root.squashfs http://${server}/${name}/root.squashfs.sig
-    imgselect kernel
-    boot
-  '';
-
-  ipxe_item_vpsadminos = name: item: ''
-    :${name}
-    menu ${name} menu
-    item ${name}_default Default runlevel
-    item ${name}_nopools Default runlevel without container imports
-    item ${name}_nostart Default runlevel without container autostart
-    item ${name}_rescue Rescue runlevel (network and sshd)
-    item ${name}_single Single-user runlevel (console only)
-    choose --default ${name}_default --timeout 5000 runlevel || goto :restart
-    goto ''${runlevel}
-
-    :${name}_default
-    ${ipxe_boot_vpsadminos { inherit name item; runlevel = "default"; }}
-
-    :${name}_nopools
-    ${ipxe_boot_vpsadminos { inherit name item; runlevel = "default"; kernelParams = [ "osctl.pools=0" ]; }}
-
-    :${name}_nostart
-    ${ipxe_boot_vpsadminos { inherit name item; runlevel = "default"; kernelParams = [ "osctl.autostart=0" ]; }}
-
-    :${name}_rescue
-    ${ipxe_boot_vpsadminos { inherit name item; runlevel = "rescue"; }}
-
-    :${name}_single
-    ${ipxe_boot_vpsadminos { inherit name item; runlevel = "single"; }}
-  '';
+  allItems = cfg.nixosItems // cfg.vpsadminosItems;
 
   concatNl = concatStringsSep "\n";
-  items_nixos = concatNl (mapAttrsToList ipxe_item_nixos cfg.nixosItems);
-  items_vpsadminos = concatNl (mapAttrsToList ipxe_item_vpsadminos cfg.vpsadminosItems);
 
-  all_items = cfg.nixosItems // cfg.vpsadminosItems;
+  # See https://wiki.syslinux.org/wiki/index.php?title=PXELINUX#Configuration
+  transformMac = mac: replaceStrings [ ":" ] [ "-" ] mac;
 
-  items_symlinks = items: concatNl (mapAttrsToList (name: x: ''
-      mkdir -p $out/${name}
-      for i in ${x.dir}/*; do
-        ln -s $i $out/${name}/$( basename $i)
-        signit $out/${name}/$( basename $i )
-      done
-    '') items);
+  symlinkItemsBoot = items: concatNl (mapAttrsToList (name: item: ''
+    mkdir -p $out/boot/${name}
+    for i in ${item.dir}/{kernel,bzImage,initrd}; do
+      ln -s $i $out/boot/${name}/$( basename $i)
+    done
+  '') items);
 
-  menu_items = concatNl (mapAttrsToList (name: x: "item ${name} ${x.menu}" )
-    (filterAttrs (const (hasAttr "menu")) all_items));
+  symlinkItemsRootfs = items: concatNl (mapAttrsToList (name: item: ''
+    mkdir -p $out/${name}
+    for i in ${item.dir}/root.squashfs; do
+      ln -s $i $out/${name}/$( basename $i)
+    done
+  '') items);
 
-  extract_mapping = name: item:
+  nginxRoot = pkgs.runCommand "nginxroot" { buildInputs = [ pkgs.openssl ]; } ''
+    mkdir -pv $out
+
+    ${symlinkItemsRootfs cfg.nixosItems}
+    ${symlinkItemsRootfs cfg.vpsadminosItems}
+  '';
+
+  extractMapping = name: item:
     map (mac: nameValuePair mac name) (item.macs or []);
 
-  extract_mappings = items:
-    flatten (mapAttrsToList extract_mapping items);
+  extractMappings = items:
+    flatten (mapAttrsToList extractMapping items);
 
-  all_mappings =
+  allMappings =
     mergeAttrs
-      (listToAttrs (flatten (map extract_mappings [ cfg.nixosItems cfg.vpsadminosItems ])))
+      (listToAttrs (flatten (map extractMappings [ cfg.nixosItems cfg.vpsadminosItems ])))
       cfg.extraMappings;
 
-  mapping_items =
-    concatNl (mapAttrsToList (mac: item: "iseq \${mac} ${mac} && goto ${item} ||") all_mappings);
+  osFragmentVar =
+    { name, item, variant, label, runlevel ? "default", kernelParams ? [] }: ''
+      LABEL ${variant}
+        MENU LABEL ${label}
+        LINUX boot/${name}/kernel
+        INITRD boot/${name}/initrd
+        APPEND httproot=http://${server}/${name}/root.squashfs systemConfig=${builtins.unsafeDiscardStringContext item.toplevel} ${toString item.kernelParams} runlevel=${runlevel} ${concatStringsSep " " kernelParams}
+    '';
 
-  ipxe_script = pkgs.writeText "script.ipxe" ''
-    #!ipxe
-    echo ${cfg.banner} stage 2
+  osFragment = name: item: ''
+    MENU TITLE ${name}
 
-    :net
-    set example_client:hex 53:54:00:70:e6:6c
-    iseq ''${mac} ''${example_client} && goto vpsadminos ||
-    ${mapping_items}
-    goto menu
+    ${osFragmentVar { inherit name item; variant = "default"; label = "Default runlevel"; }}
 
-    :menu
-    :restart
-    menu iPXE boot menu
-    item normal Boot normally
-    ${optionalString cfg.includeNetbootxyz "item netbootxyz netboot.xyz"}
-    ${menu_items}
-    item loop Start iPXE shell
-    item off Shutdown
-    item reset Reboot
-    choose --default vpsadminos --timeout 5000 res || goto restart
-    goto ''${res}
+    ${osFragmentVar { inherit name item; variant = "nopools"; label = "Default runlevel without container imports"; kernelParams = [ "osctl.pools=0" ]; }}
 
-    :off
-    poweroff
-    goto off
-    :reset
-    reboot
-    goto reset
+    ${osFragmentVar { inherit name item; variant = "nostart"; label = "Default runlevel without container autostart"; kernelParams = [ "osctl.autostart=0" ]; }}
 
-    :loop
-    login || goto cancelled
+    ${osFragmentVar { inherit name item; variant = "rescue"; label = "Rescue runlevel (network and sshd)"; runlevel = "rescue"; }}
 
-    iseq ''${password} ${cfg.password} && goto is_correct ||
-    echo password wrong
-    sleep 5
-    goto loop
+    ${osFragmentVar { inherit name item; variant = "single"; label = "Single-user runlevel (console only)"; runlevel = "single"; }}
 
-    :cancelled
-    echo you gave up, goodbye
-    sleep 5
-    poweroff
-    goto cancelled
-
-    :is_correct
-    shell
-
-    ${optionalString cfg.includeNetbootxyz ''
-      :netbootxyz
-      imgtrust --allow
-      chain http://boot.netboot.xyz
-    ''}
-
-    ${items_vpsadminos}
-    ${items_nixos}
+    LABEL mainmenu
+      MENU LABEL Main Menu
+      KERNEL menu.c32
+      APPEND pxelinux.cfg/default
   '';
 
-  ipxe = pkgs.lib.overrideDerivation pkgs.ipxe (x: rec {
-    script = pkgs.writeText "embed.ipxe" ''
-      #!ipxe
-      echo ${cfg.banner} stage 1
-      imgtrust ${optionalString (!cfg.includeNetbootxyz) "--permanent"}
-      dhcp
-      imgfetch http://${server}/script.ipxe
-      imgverify script.ipxe http://${server}/script.ipxe.sig
-      chain script.ipxe
-      echo temporary debug shell
-      shell
-    '';
-    nativeBuildInputs = x.nativeBuildInputs ++ [ pkgs.openssl ];
-    makeFlags = x.makeFlags ++ [
-      "EMBED=${script}"
-      "TRUST=${caCert}"
-      "CERT=${caCodesign},${caCert}"
-    ];
+  osItemIncludeConfigs = mapAttrs (name: item:
+    pkgs.writeText "${name}.cfg" (osFragment name item)
+  ) cfg.vpsadminosItems;
 
-    enabledOptions = x.enabledOptions ++ [ "CONSOLE_SERIAL" "POWEROFF_CMD" "IMAGE_TRUST_CMD" ];
-  });
+  osItemDirectConfigs = mapAttrs (name: item: pkgs.writeText "${name}.cfg" ''
+    DEFAULT menu.c32
+    TIMEOUT 50
+    ${osFragment name item}
+  '') cfg.vpsadminosItems;
+
+  osMenuFragment = mapAttrsToList (name: item: ''
+    LABEL ${name}
+      MENU LABEL ${name}
+      KERNEL menu.c32
+      APPEND pxeserver/${name}.cfg
+  '') cfg.vpsadminosItems;
+
+  pxeOsMenu = pkgs.writeText "vpsadminos.cfg" ''
+    MENU TITLE vpsAdminOS
+
+    LABEL mainmenu
+      MENU LABEL Main Menu
+      KERNEL menu.c32
+      APPEND pxelinux.cfg/default
+
+    ${concatNl osMenuFragment}
+  '';
+
+  nixosMenuFragment = mapAttrsToList (name: item: ''
+    LABEL ${name}
+      MENU LABEL ${item.menu or name}
+      LINUX boot/${server}/${name}/bzImage
+      INITRD boot/${server}/${name}/initrd
+      APPEND init=${builtins.unsafeDiscardStringContext item.toplevel}/init loglevel=7
+  '') cfg.nixosItems;
+
+  pxeNixosMenu = pkgs.writeText "nixos.cfg" ''
+    MENU TITLE NixOS
+
+    LABEL mainmenu
+      MENU LABEL Main Menu
+      KERNEL menu.c32
+      APPEND pxelinux.cfg/default
+
+    ${concatNl nixosMenuFragment}
+  '';
 
   pxeLinuxDefault = pkgs.writeText "default" ''
-    DEFAULT ipxe
-    LABEL ipxe
-    KERNEL ipxe.lkrn
-    '';
+    DEFAULT menu.c32
+    PROMPT 0
+    TIMEOUT 0
+    MENU TITLE ${config.networking.hostName}
 
-  tftp_root = pkgs.runCommand "tftproot" {} ''
-    mkdir -pv $out
-    mkdir $out/pxelinux.cfg
+    LABEL nixos
+      MENU LABEL NixOS
+      KERNEL menu.c32
+      APPEND pxeserver/nixos.cfg
 
-    ln -s ${pxeLinuxDefault} $out/pxelinux.cfg/default
+    LABEL vpsadminos
+      MENU LABEL vpsAdminOS
+      KERNEL menu.c32
+      APPEND pxeserver/vpsadminos.cfg
 
-    ln -s ${pkgs.syslinux}/share/syslinux/pxelinux.0 $out/pxelinux.0
-    ln -s ${pkgs.syslinux}/share/syslinux/ldlinux.c32 $out/ldlinux.c32
+    LABEL local_boot
+      MENU LABEL Local Boot
+      LOCALBOOT 0
 
-    ln -s ${ipxe}/ipxe.lkrn $out/ipxe.lkrn
+    LABEL warm_reboot
+      MENU LABEL Warm Reboot
+      KERNEL reboot.c32
+      APPEND --warm
 
-    #cp -vi ${ipxe}/undionly.kpxe $out/undionly.kpxe
+    LABEL cold_reboot
+      MENU LABEL Cold Reboot
+      KERNEL reboot.c32
   '';
 
-  nginx_root = pkgs.runCommand "nginxroot" { buildInputs = [ pkgs.openssl ]; } ''
+  tftpRoot = pkgs.runCommand "tftproot" {} ''
     mkdir -pv $out
+    mkdir $out/pxelinux.cfg $out/pxeserver
 
-    ln -sv ${ipxe_script} $out/script.ipxe
-    function signit {
-      openssl cms -sign -binary -noattr -in $1 -signer ${secretsDir}/codesign.crt -inkey ${secretsDir}/codesign.key -certfile ${secretsDir}/root.pem -outform DER -out ''${1}.sig
-    }
-    signit $out/script.ipxe
+    # pxelinux programs
+    for prog in pxelinux.0 ldlinux.c32 libcom32.c32 libutil.c32 menu.c32 reboot.c32 ; do
+      ln -s ${pkgs.syslinux}/share/syslinux/$prog $out/$prog
+    done
 
-    ${items_symlinks cfg.nixosItems}
-    ${items_symlinks cfg.vpsadminosItems}
+    # The default configuration
+    ln -s ${pxeLinuxDefault} $out/pxelinux.cfg/default
+
+    # Per-mac address configs
+    ${concatNl (mapAttrsToList (mac: name: ''
+      ln -s ${osItemDirectConfigs.${name}} $out/pxelinux.cfg/01-${transformMac mac}
+    '') allMappings)}
+
+    # Grouped menus for NixOS and vpsAdminOS items
+    ln -s ${pxeNixosMenu} $out/pxeserver/nixos.cfg
+    ln -s ${pxeOsMenu} $out/pxeserver/vpsadminos.cfg
+
+    ${concatNl (mapAttrsToList (name: cfg: ''
+      ln -s ${cfg} $out/pxeserver/${name}.cfg
+    '') osItemIncludeConfigs)}
+
+    # Links for kernels and initrd
+    ${symlinkItemsBoot cfg.nixosItems}
+    ${symlinkItemsBoot cfg.vpsadminosItems}
   '';
 in
 {
@@ -271,19 +247,25 @@ in
   };
 
   config = mkIf cfg.enable {
-    networking.firewall.allowedUDPPorts = [ 68 69 ];
-    networking.firewall.allowedTCPPorts = [ 80 ] ++ lib.optional cfg.acmeSSL 443;
+    networking.firewall = {
+      allowedTCPPorts = [ 80 ] ++ lib.optional cfg.acmeSSL 443;
+
+      extraCommands = mkIf (cfg.allowedIPRanges != []) (concatNl (map (net: ''
+        iptables -A nixos-fw -p udp -s ${net} --dport 68 -j nixos-fw-accept
+        iptables -A nixos-fw -p udp -s ${net} --dport 69 -j nixos-fw-accept
+      '') cfg.allowedIPRanges));
+    };
 
     services.tftpd = {
-        enable = true;
-        path = tftp_root;
+      enable = true;
+      path = tftpRoot;
     };
 
     services.nginx = {
       enable = true;
       virtualHosts = {
         "${server}" = {
-          root = nginx_root;
+          root = nginxRoot;
           addSSL = cfg.acmeSSL;
           enableACME = cfg.acmeSSL;
           locations = {
