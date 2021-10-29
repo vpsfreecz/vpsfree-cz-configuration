0bdd02fba21e8da22d8ca79923a4eca245bc2548
Jakub Skokan <jakub.skokan@havefun.cz>
2021-10-29 09:09:11 +0200

    vpsadmin/common/database: allow other vpsadmin components to connect

--- cluster/cz.vpsfree/vpsadmin/common/database.nix
+++ cluster/cz.vpsfree/vpsadmin/common/database.nix
@@ -1,5 +1,20 @@
 { pkgs, lib, config, confLib, confData, ... }:
-{
+let
+  api1 = confLib.findConfig {
+    cluster = config.cluster;
+    name = "cz.vpsfree/vpsadmin/int.api1";
+  };
+
+  api2 = confLib.findConfig {
+    cluster = config.cluster;
+    name = "cz.vpsfree/vpsadmin/int.api2";
+  };
+
+  proxyPrg = confLib.findConfig {
+    cluster = config.cluster;
+    name = "cz.vpsfree/containers/prg/proxy";
+  };
+in {
   vpsadmin.database = {
     enable = true;
 
@@ -12,6 +27,10 @@
         ) confData.vpsadmin.networks.management.ipv4;
 
         others = [
+          "${api1.addresses.primary.address}/32"
+          "${api2.addresses.primary.address}/32"
+          "${proxyPrg.addresses.primary.address}/32"
+          "172.16.8.5/32" # vpsadmin.int
           "37.205.8.141/32" # utils.vpsfree.cz
         ];
       in management ++ others;
