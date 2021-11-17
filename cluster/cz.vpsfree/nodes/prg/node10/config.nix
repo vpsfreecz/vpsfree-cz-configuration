d00cb103d350120d7cf55b01ebc29c7a5a772987
Jakub Skokan <jakub.skokan@havefun.cz>
2021-11-17 09:51:04 +0100

    node10.prg: update macs for netboot and disks for zpool

--- cluster/cz.vpsfree/nodes/prg/node10/config.nix
+++ cluster/cz.vpsfree/nodes/prg/node10/config.nix
@@ -7,9 +7,10 @@
   boot.zfs.pools = {
     tank = {
       install = true;
-      wipe = [ "sda" "sdb" "sdc" "sdd" "sde" "sdf" "sdg" "sdh" ];
+      wipe = [ "sda" "sdb" "sdc" "sdd" "sde" "sdf" "sdg" "sdh" "sdi" "sdj" ];
       layout = [
         { type = "mirror"; devices = [ "sdc" "sdd" ]; }
+        { type = "mirror"; devices = [ "sde" "sdf" ]; }
         { type = "mirror"; devices = [ "sdg" "sdh" ]; }
         { type = "mirror"; devices = [ "sdi" "sdj" ]; }
       ];
