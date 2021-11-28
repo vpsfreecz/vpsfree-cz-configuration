2418f5ea33a1ead6e7868986304d9087b26920b6
Jakub Skokan <jakub.skokan@havefun.cz>
2021-11-28 10:05:30 +0100

    image-repository: add alpine 3.15, remove 3.12

--- configs/image-repository.nix
+++ configs/image-repository.nix
@@ -31,9 +31,9 @@
       };
 
       alpine = {
-        "3.12" = {};
         "3.13" = {};
-        "3.14" = { tags = [ "latest" "stable" ]; };
+        "3.14" = {};
+        "3.15" = { tags = [ "latest" "stable" ]; };
       };
 
       arch.rolling = { name = "arch"; tags = [ "latest" "stable" ]; };
