fd29d1fbbb75aa88d4de667fa9e5b7d7715a5095
Pavel Snajdr <snajpa@snajpa.net>
2021-12-09 15:20:31 +0100

    meet: meet-dev devel setup

--- data/meet.nix
+++ data/meet.nix
@@ -1,4 +1,23 @@
 {
+  vpsfree-dev = rec {
+    host = "meet-dev.vpsfree.cz";
+    url = "https://${host}";
+    alias = host;
+    videoBridges = {
+      jvb1 = "37.205.14.138";
+      jvb2 = "37.205.14.150";
+      jvb3 = "37.205.14.163";
+      jvb4 = "37.205.14.178";
+      jvb5 = "37.205.14.182";
+      jvb6 = "37.205.14.207";
+      jvb7 = "37.205.14.214";
+      jvb8 = "37.205.14.219";
+      jvb9 = "37.205.14.231";
+      jvb10 = "37.205.14.250";
+      jvb11 = "37.205.13.90";
+    };
+  };
+
   vpsfree = rec {
     host = "meet.vpsfree.cz";
     url = "https://${host}";
