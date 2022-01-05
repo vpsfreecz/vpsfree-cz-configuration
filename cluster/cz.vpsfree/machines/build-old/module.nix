a31f4b6ede29f68293d8db82dab298c2cd6de26a
Jakub Skokan <jakub.skokan@havefun.cz>
2022-01-05 12:51:49 +0100

    cluster: fix build-old name

--- cluster/cz.vpsfree/machines/build-old/module.nix
+++ cluster/cz.vpsfree/machines/build-old/module.nix
@@ -3,7 +3,7 @@
   cluster."cz.vpsfree/machines/build-old" = rec {
     spin = "vpsadminos";
     swpins.channels = [ "os-staging" "nixos-stable" ];
-    host = { name = "build"; domain = "vpsfree.cz"; target = "172.16.254.4"; };
+    host = { name = "build-old"; domain = "vpsfree.cz"; target = "172.16.254.4"; };
     addresses.primary = { address = "172.16.254.4"; prefix = 32; };
     services.node-exporter = {};
   };
