6389551a23a022f7934aca07236f0fa6b6364d21
Jakub Skokan <jakub.skokan@havefun.cz>
2022-03-20 13:55:04 +0100

    packages: update vpsfbot to a6ae4405
    Includes end-of-day message in the log.

--- packages/vpsfree-irc-bot/default.nix
+++ packages/vpsfree-irc-bot/default.nix
@@ -3,8 +3,8 @@ let
   vpsfbot = fetchFromGitHub {
     owner = "vpsfreecz";
     repo = "vpsfree-irc-bot";
-    rev = "7c619155e9e9f2a6a5401e22b5477c1a6af151f5";
-    sha256 = "sha256:1y83ld5x6mvrb5mdbhafaid52522cjbhx63fgj64fvdp9y8g4sbb";
+    rev = "a6ae4405a3cbfc9a9c63e8697668d729503e67fd";
+    sha256 = "sha256:0xs1cwbyjr26rjn329xzkw0y80861y8z79hf4ig07mvszb6637fx";
   };
 in bundlerEnv {
   name = "vpsfree-irc-bot";
