9fe29dcc7e6b5b456ba1c3458a1b03dea48fdff0
Jakub Skokan <jakub.skokan@havefun.cz>
2022-05-27 16:47:47 +0200

    apu: add confctl tag apu

--- cluster/cz.vpsfree/machines/brq/apu/module.nix
+++ cluster/cz.vpsfree/machines/brq/apu/module.nix
@@ -5,6 +5,7 @@
     swpins.channels = [ "nixos-stable" ];
     host = { name = "apu"; location = "int.brq"; domain = "vpsfree.cz"; target = "172.19.254.254"; };
     addresses.primary = { address = "172.19.254.254"; prefix = 24; };
+    tags = [ "apu" ];
     services = {
       node-exporter = {};
       sachet = {};
--- cluster/cz.vpsfree/machines/prg/apu/module.nix
+++ cluster/cz.vpsfree/machines/prg/apu/module.nix
@@ -5,6 +5,7 @@
     swpins.channels = [ "nixos-stable" ];
     host = { name = "apu"; location = "int.prg"; domain = "vpsfree.cz"; target = "172.16.254.254"; };
     addresses.primary = { address = "172.16.254.254"; prefix = 24; };
+    tags = [ "apu" ];
     services = {
       node-exporter = {};
       sachet = {};
