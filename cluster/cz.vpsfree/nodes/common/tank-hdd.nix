145eaa84ddcd1ce3f49b22ed63468a0ad98a28d0
Jakub Skokan <jakub.skokan@havefun.cz>
2022-06-07 20:11:55 +0200

    nodes: import common/tank-hdd.nix on hdd nodes

---
+++ cluster/cz.vpsfree/nodes/common/tank-hdd.nix
@@ -0,0 +1,8 @@
+{ config, lib, pkgs, ...}:
+{
+  # Scrub HDD-backed nodes only between 23h-07h
+  boot.zfs.pools.tank.scrub = {
+    pauseIntervals = [ "0 7 * * *" ];
+    resumeIntervals = [ "0 23 * * *" ];
+  };
+}
--- cluster/cz.vpsfree/nodes/prg/node8/config.nix
+++ cluster/cz.vpsfree/nodes/prg/node8/config.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ../common.nix
+    ../../common/tank-hdd.nix
   ];
 
   boot.zfs.pools = {
