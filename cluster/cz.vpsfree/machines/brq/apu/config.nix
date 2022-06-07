b40b5b9e033ef50f31284504eda070e9a80cf171
Jakub Skokan <jakub.skokan@havefun.cz>
2022-06-07 08:42:16 +0200

    apu.brq: add q66

--- cluster/cz.vpsfree/machines/brq/apu/config.nix
+++ cluster/cz.vpsfree/machines/brq/apu/config.nix
@@ -12,7 +12,20 @@
     SUBSYSTEM=="tty", ATTRS{serial}=="001704A7", OWNER="snajpa", GROUP="tty-vpsf-net", SYMLINK+="ttyUSB-brq-1-12-tor2-s4048"
     SUBSYSTEM=="tty", ATTRS{serial}=="00131468", OWNER="snajpa", GROUP="tty-vpsf-net", SYMLINK+="ttyUSB-brq-1-12-edg1-s4048"
     SUBSYSTEM=="tty", ATTRS{serial}=="00170075", OWNER="snajpa", GROUP="tty-vpsf-net", SYMLINK+="ttyUSB-brq-1-12-edg2-s4048"
+    SUBSYSTEM=="tty", ATTRS{serial}=="AC025AG5", OWNER="q66",    GROUP="q66",          SYMLINK+="ttyUSB-q66"
   '';
+
+  users.groups = {
+    "q66".members = [ "q66" ];
+  };
+
+  users.users = {
+    q66 = {
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMiZG44MWYMw++jjy38C+u/nqIGnAfoy4Jy79r0FhL8n daniel@octaforge.org" ];
+    };
+  };
 
   networking.interfaces.enp1s0.useDHCP = false;
   networking.interfaces.enp1s0.ipv4.addresses = [
