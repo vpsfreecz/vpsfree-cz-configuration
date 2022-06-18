9c89a5a22787f3c02b8758aa31d6b9103f0b1155
Jakub Skokan <jakub.skokan@havefun.cz>
2022-06-18 16:49:30 +0200

    packages: update vpsf-status to bd3d2f23

--- packages/vpsf-status/default.nix
+++ packages/vpsf-status/default.nix
@@ -2,14 +2,14 @@
 let
   owner = "vpsfreecz";
   repo = "vpsf-status";
-  rev = "634547790c4e2a70fa2d0aeec6ca61a6d71d7cf4";
+  rev = "bd3d2f2372f2f2f579b5f6c701635ddf35c2556c";
 in buildGo117Module rec {
   name = "vpsf-status-${version}";
   version = lib.substring 0 7 rev;
 
   src = fetchFromGitHub {
     inherit owner repo rev;
-    sha256 = "sha256:0jhggilrzgl26zpy9jhdk2ir4c7rwydnvlwcqj83cdnlhnp96bn3";
+    sha256 = "sha256-OUtgXowi2xcbbBSNs71Gg/YcKjgiYuUjeIO8qRU4CaY=";
   };
 
   vendorSha256 = "sha256:1is1g69r3xpipcy1pk5dwjxw5ywyk2miqr06smg2hp89slkqswba";
