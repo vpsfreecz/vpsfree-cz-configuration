89ddd1f0dc398e48292ffcefd9677f4cbc4fb4eb
Jakub Skokan <jakub.skokan@havefun.cz>
2022-06-27 17:38:47 +0200

    packages/vpsfree-irc-bot: update to aa8a289e9

--- packages/vpsfree-irc-bot/default.nix
+++ packages/vpsfree-irc-bot/default.nix
@@ -3,8 +3,8 @@ let
   vpsfbot = fetchFromGitHub {
     owner = "vpsfreecz";
     repo = "vpsfree-irc-bot";
-    rev = "160cac73685eedd098e2af16e8840bdcfb304f32";
-    sha256 = "sha256:1b9pnirp1am3q9m8rir96j37g7h0nn02b1ibxskalamp92l3gdmz";
+    rev = "aa8a289e94f05afaeb907393b3e96568259ad7bf";
+    sha256 = "sha256-8jFwtIgESIS7KJciOrd+9iDePRNyqyg3z8UzGmsrrbQ=";
   };
 in bundlerEnv {
   name = "vpsfree-irc-bot";
