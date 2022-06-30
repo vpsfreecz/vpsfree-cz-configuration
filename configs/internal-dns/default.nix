bd53533644b043b6cfc11f88484497a91c89fa81
Jakub Skokan <jakub.skokan@havefun.cz>
2022-06-30 11:05:59 +0200

    internal-dns: allow access from container/management networks

--- configs/internal-dns/default.nix
+++ configs/internal-dns/default.nix
@@ -1,9 +1,18 @@
-{ config, lib, pkgs, confMachine, ... }:
+{ config, lib, pkgs, confData, confMachine, ... }:
+with lib;
 let
   mkZoneFile = file: pkgs.substituteAll {
     src = file;
     fqdn = confMachine.host.fqdn;
   };
+
+  formatNetworks = list: map (net: "${net.address}/${toString net.prefix}") list;
+
+  containerNetworks = formatNetworks confData.vpsadmin.networks.containers.ipv4;
+
+  managementNetworks = formatNetworks confData.vpsadmin.networks.management.ipv4;
+
+  allNetworks = containerNetworks ++ managementNetworks;
 in {
   services.bind = {
     enable = true;
@@ -14,7 +23,7 @@ in {
     cacheNetworks = [
       "127.0.0.0/24"
       "172.16.0.0/12"
-    ];
+    ] ++ allNetworks;
     zones = [
       {
         name = "vpsfree.cz.";
@@ -30,6 +39,10 @@ in {
   };
 
   networking.firewall.extraCommands = ''
-    iptables -A INPUT -p udp -m udp -s 172.16.0.0/12 --dport 53 -j ACCEPT
-  '';
+    iptables -A nixos-fw -p udp -s 172.16.0.0/12 --dport 53 -j nixos-fw-accept
+    iptables -A nixos-fw -p tcp -s 172.16.0.0/12 --dport 53 -j nixos-fw-accept
+  '' + (concatMapStringsSep "\n" (net: ''
+    iptables -A nixos-fw -p udp -s ${net} --dport 53 -j nixos-fw-accept
+    iptables -A nixos-fw -p tcp -s ${net} --dport 53 -j nixos-fw-accept
+  '') allNetworks);
 }
