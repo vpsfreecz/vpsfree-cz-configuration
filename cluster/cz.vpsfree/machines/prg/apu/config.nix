f13380b77f86e8f1631da2a20394fed634b226a3
Jakub Skokan <jakub.skokan@havefun.cz>
2022-07-22 15:44:53 +0200

    apu.prg: add udev rule for ttyUSB-dev-s4048

--- cluster/cz.vpsfree/machines/prg/apu/config.nix
+++ cluster/cz.vpsfree/machines/prg/apu/config.nix
@@ -14,6 +14,7 @@
     SUBSYSTEM=="tty", ATTRS{serial}=="000C5978", OWNER="snajpa", GROUP="tty-vpsf-net", SYMLINK+="ttyUSB-prg-14-7-edg2-s4048"
     SUBSYSTEM=="tty", ATTRS{serial}=="000CBF24", OWNER="snajpa", GROUP="tty-vpsf-net", SYMLINK+="ttyUSB-prg-14-7-tor1-s4048"
     SUBSYSTEM=="tty", ATTRS{serial}=="000BF9F8", OWNER="snajpa", GROUP="tty-vpsf-net", SYMLINK+="ttyUSB-prg-14-7-tor2-s4048"
+    SUBSYSTEM=="tty", ATTRS{serial}=="001333B4", OWNER="snajpa", GROUP="tty-vpsf-net", SYMLINK+="ttyUSB-dev-s4048"
   '';
 
   networking.interfaces.enp1s0.useDHCP = false;
