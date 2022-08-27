4a1580f9f8558716f7b399a846dff4d4be7f5f19
Jakub Skokan <jakub.skokan@havefun.cz>
2022-08-27 17:00:49 +0200

    network-graphs: add edge1.prg

--- modules/services/network-graphs/fetch-graphs.rb
+++ modules/services/network-graphs/fetch-graphs.rb
@@ -12,6 +12,7 @@ now = Time.now.to_i
 day_ago = now - 24 * 60 * 60
 
 files = {
+  'edge1_prg_vpsfree.gif' => "http://172.16.100.2/graphs/iface/master-vlan128-uplink/daily.gif",
   'edge2_prg_vpsfree.png' => "http://172.16.4.4/graph.php?to=#{now}&id=62&type=port_bits&from=#{day_ago}&height=250&width=550",
   'rtr1_brq_vpsfree.gif' => "http://172.19.0.2/graphs/iface/sfpplus1-master/daily.gif",
 }
