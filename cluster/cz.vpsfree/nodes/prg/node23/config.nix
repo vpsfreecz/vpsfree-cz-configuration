7f47ab62384a9b642abe01b45d1e9a3cb91db309
Jakub Skokan <jakub.skokan@havefun.cz>
2022-09-16 12:32:55 +0200

    cluster: add node23.prg

---
+++ cluster/cz.vpsfree/nodes/prg/node23/config.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, lib, ...}:
+{
+  imports = [
+    ../common.nix
+  ];
+
+  boot.zfs.pools = {
+    tank = {
+      install = true;
+      wipe = [ "sda" "sdb" "sdc" "sdd" "sde" "sdf" "sdg" "sdh" "sdi" "sdj" "sdk" "sdl" ];
+      layout = [
+        { type = "raidz"; devices = [ "sda" "sdb" "sdc" "sdd" "sde" "sdf" ]; }
+        { type = "raidz"; devices = [ "sdg" "sdh" "sdi" "sdj" "sdk" "sdl" ]; }
+      ];
+      properties = {
+        ashift = "12";
+      };
+    };
+  };
+
+  osctl.pools.tank = {
+    parallelStart = 20;
+    parallelStop = 40;
+  };
+
+  swapDevices = [
+  ];
+}
---
+++ cluster/cz.vpsfree/nodes/prg/node23/module.nix
@@ -0,0 +1,90 @@
+{ config, ... }:
+let
+  allAddresses = {
+    primary = { address = "172.16.0.33"; prefix = 32; };
+    teng0 = {
+      v4 = [
+        { address = "172.16.251.10"; prefix = 30; }
+      ];
+      v6 = [
+        { address = "2a03:3b40:42:2:3::2"; prefix = 80; }
+      ];
+    };
+    teng1 = {
+      v4 = [
+        { address = "172.16.250.10"; prefix = 30; }
+      ];
+      v6 = [
+        { address = "2a03:3b40:42:3:3::2"; prefix = 80; }
+      ];
+    };
+  };
+in {
+  cluster."cz.vpsfree/nodes/prg/node23" = rec {
+    spin = "vpsadminos";
+
+    swpins.channels = [ "prod-22.09" ];
+
+    node = {
+      id = 124;
+      role = "hypervisor";
+    };
+
+    host = {
+      name = "node23";
+      location = "prg";
+      domain = "vpsfree.cz";
+    };
+
+    netboot = {
+      enable = true;
+      macs = [
+        "f4:02:70:d9:6b:8e"
+        "f4:02:70:d9:6b:8f"
+      ];
+    };
+
+    addresses = with allAddresses; {
+      inherit primary;
+      v4 = teng0.v4 ++ teng1.v4;
+      v6 = teng0.v6 ++ teng1.v6;
+    };
+
+    osNode = {
+      networking = {
+        interfaces = {
+          names = {
+            teng0 = "68:05:ca:b1:c6:e4";
+            teng1 = "68:05:ca:b1:c6:e5";
+          };
+          addresses = {
+            inherit (allAddresses) teng0 teng1;
+          };
+        };
+
+        bird = {
+          as = 4200001003;
+          routerId = "172.16.0.28";
+          bgpNeighbours = {
+            v4 = [
+              { address = "172.16.251.9"; as = 4200001901; }
+              { address = "172.16.250.9"; as = 4200001902; }
+            ];
+            v6 = [
+              { address = "2a03:3b40:42:2:03::1"; as = 4200001901; }
+              { address = "2a03:3b40:42:3:03::1"; as = 4200001902; }
+            ];
+          };
+        };
+
+        virtIP = addresses.primary;
+      };
+    };
+
+    services = {
+      node-exporter = {};
+      osctl-exporter = {};
+      vpsadmin-console = {};
+    };
+  };
+}
