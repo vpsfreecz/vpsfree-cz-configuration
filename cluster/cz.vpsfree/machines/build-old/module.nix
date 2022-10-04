82cbbc3cc216595ead161d0c759ffe4c36766018
Jakub Skokan <jakub.skokan@havefun.cz>
2022-10-04 19:02:05 +0200

    build-old: remove from monitoring

--- cluster/cz.vpsfree/machines/build-old/module.nix
+++ cluster/cz.vpsfree/machines/build-old/module.nix
@@ -5,6 +5,7 @@
     swpins.channels = [ "os-staging" "nixos-stable" ];
     host = { name = "build-old"; domain = "vpsfree.cz"; target = "172.16.254.4"; };
     addresses.primary = { address = "172.16.254.4"; prefix = 32; };
+    monitoring.enable = false;
     services.node-exporter = {};
   };
 }
