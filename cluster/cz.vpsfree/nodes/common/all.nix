c4dfa357a08457bec54aa6104ea5edcbd5e608e1
Jakub Skokan <jakub.skokan@havefun.cz>
2022-11-05 20:39:32 +0100

    nodes/common/all: increase mbuffer send block size to 128k

--- cluster/cz.vpsfree/nodes/common/all.nix
+++ cluster/cz.vpsfree/nodes/common/all.nix
@@ -142,7 +142,7 @@
   osctld.settings = {
     send_receive = {
       send_mbuffer = {
-        block_size = "32k";
+        block_size = "128k";
         buffer_size = "512M";
         start_writing_at = 5;
       };
