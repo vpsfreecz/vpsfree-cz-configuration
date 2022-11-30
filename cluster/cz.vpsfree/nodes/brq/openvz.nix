91c1fe2da159175a29edd2a91ad3f4094f8a9f0b
Jakub Skokan <jakub.skokan@havefun.cz>
2022-11-30 17:19:40 +0100

    cluster: remove node1.brq

--- cluster/cz.vpsfree/nodes/brq/openvz.nix
+++ cluster/cz.vpsfree/nodes/brq/openvz.nix
@@ -1,22 +1,6 @@
 { config, lib, ... }:
 {
   cluster = {
-    "cz.vpsfree/nodes/brq/node1" = {
-      spin = "openvz";
-      node = {
-        id = 210;
-        role = "hypervisor";
-      };
-      host = {
-        name = "node1";
-        location = "brq";
-        domain = "vpsfree.cz";
-      };
-      addresses.primary = { address = "172.19.0.10"; prefix = 23; };
-      vzNode.role = "hypervisor";
-      services.node-exporter = {};
-    };
-
     "cz.vpsfree/nodes/brq/node3" = {
       spin = "openvz";
       node = {
