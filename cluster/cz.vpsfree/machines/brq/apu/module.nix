605820fbd90abac62ecb41609f8477eee48f629f
Jakub Skokan <jakub.skokan@havefun.cz>
2023-01-14 13:05:00 +0100

    apus: add swpins channel os-staging

--- cluster/cz.vpsfree/machines/brq/apu/module.nix
+++ cluster/cz.vpsfree/machines/brq/apu/module.nix
@@ -2,7 +2,11 @@
 {
   cluster."cz.vpsfree/machines/brq/apu" = rec {
     spin = "nixos";
-    swpins.channels = [ "nixos-stable" ];
+
+    # os-staging is needed to keep the channels same with the build machine, so
+    # that confctl can evaluate it in one run when deploying all pxe machines.
+    swpins.channels = [ "nixos-stable" "os-staging" ];
+
     host = { name = "apu"; location = "int.brq"; domain = "vpsfree.cz"; target = "172.19.254.254"; };
     addresses = {
       v4 = [ { address = "172.19.254.254"; prefix = 24; } ];
--- cluster/cz.vpsfree/machines/prg/apu/module.nix
+++ cluster/cz.vpsfree/machines/prg/apu/module.nix
@@ -2,7 +2,11 @@
 {
   cluster."cz.vpsfree/machines/prg/apu" = rec {
     spin = "nixos";
-    swpins.channels = [ "nixos-stable" ];
+
+    # os-staging is needed to keep the channels same with the build machine, so
+    # that confctl can evaluate it in one run when deploying all pxe machines.
+    swpins.channels = [ "nixos-stable" "os-staging" ];
+
     host = { name = "apu"; location = "int.prg"; domain = "vpsfree.cz"; target = "172.16.254.254"; };
     addresses = {
       v4 = [ { address = "172.16.254.254"; prefix = 24; } ];
