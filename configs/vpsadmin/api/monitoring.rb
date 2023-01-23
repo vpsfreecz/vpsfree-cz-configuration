39571690e1269ee60ee501a852365b5b5b06d703
Jakub Skokan <jakub.skokan@havefun.cz>
2023-01-23 16:30:25 +0100

    vpsadmin-config: monitoring: add rules for zombie processes

--- configs/vpsadmin/api/monitoring.rb
+++ configs/vpsadmin/api/monitoring.rb
@@ -24,6 +24,30 @@ MailTemplate.register :alert_role_diskspace_state_pool,
     base_url: 'URL to the web UI',
   }, roles: %i(admin)
 
+MailTemplate.register :alert_user_zombie_processes_state,
+  name: "alert_user_zombie_processes_%{state}", params: {
+    state: 'event state',
+  }, vars: {
+    event: 'MonitoredEvent',
+    vps: ::Vps,
+    zombie_process_count: Integer,
+    threshold: Integer,
+    user: ::User,
+    base_url: 'URL to the web UI',
+  }, roles: %i(admin)
+
+MailTemplate.register :alert_user_zombie_processes_restart,
+  name: "alert_user_zombie_processes_restart", vars: {
+    event: 'MonitoredEvent',
+    vps: ::Vps,
+    zombie_process_count: Integer,
+    threshold: Integer,
+    finish_weekday: Integer,
+    finish_minutes: Integer,
+    user: ::User,
+    base_url: 'URL to the web UI',
+  }, roles: %i(admin)
+
 VpsAdmin::API::Plugins::Monitoring.config do
   # Action definitions
   action :alert_user do |event|
@@ -120,6 +144,92 @@ VpsAdmin::API::Plugins::Monitoring.config do
     opts[:message_id] = msg_id
 
     mail(:alert_role_event_state, opts)
+  end
+
+  action :alert_user_zombie_processes do |event|
+    threshold = 10000
+    vps = event.object
+
+    mail_vars = {
+      event: event,
+      vps: vps,
+      zombie_process_count: vps.zombie_process_count,
+      threshold: threshold,
+      user: event.user,
+      base_url: ::SysConfig.get('webui', 'base_url'),
+    }
+
+    if vps.zombie_process_count > threshold
+      # Plan restart unless one was already planned
+      next if event.action_state && event.action_state['restart_planned']
+
+      finish_weekday = (Time.now + 24*60*60).wday
+      finish_minutes = 4*60 + rand(35..55) # 04:35-55
+
+      opts = {
+        user: event.user,
+        vars: mail_vars.merge({
+          finish_weekday: finish_weekday,
+          finish_minutes: finish_minutes,
+        }),
+      }
+
+      lock(vps)
+
+      mail(:alert_user_zombie_processes_restart, opts)
+
+      append_t(
+        Transactions::MaintenanceWindow::Wait,
+        args: [vps, 15],
+        kwargs: {
+          maintenance_windows: ::VpsMaintenanceWindow.make_for(
+            vps,
+            finish_weekday: finish_weekday,
+            finish_minutes: finish_minutes,
+          )
+        },
+      )
+
+      append_t(Transactions::Vps::Restart, args: [vps])
+
+      event.action_state ||= {}
+      event.action_state['restart_planned'] = true
+      event.save!
+
+    else
+      # Alert the user about too many zombies once per day
+      last_alert =
+        if event.action_state && event.action_state['last_alert']
+          Time.at(event.action_state['last_alert'])
+        else
+          nil
+        end
+
+      next if last_alert && last_alert + 24*60*60 > Time.now
+
+      opts = {
+        params: {
+          state: event.state == 'acknowledged' ? 'confirmed' : event.state,
+        },
+        user: event.user,
+        vars: mail_vars,
+      }
+
+      if event.state == 'closed'
+        msg_id = "<vpsadmin-monitoring-alert-#{event.id}-#{event.state}@vpsadmin.vpsfree.cz>"
+        rpl_to = "<vpsadmin-monitoring-alert-#{event.id}-confirmed@vpsadmin.vpsfree.cz>"
+        opts[:in_reply_to] = rpl_to
+        opts[:references] = rpl_to
+      else
+        msg_id = "<vpsadmin-monitoring-alert-#{event.id}-confirmed@vpsadmin.vpsfree.cz>"
+      end
+
+      mail(:alert_user_zombie_processes_state, opts)
+
+      event.action_state ||= {}
+      event.action_state['last_alert'] = Time.now.to_i
+      event.save!
+    end
   end
 
   # Monitors
@@ -266,5 +376,39 @@ VpsAdmin::API::Plugins::Monitoring.config do
     value { |tr| tr.bytes_all }
     check { |tr, v| v < 10 * 1024*1024*1024*1024 }
     action :alert_admins
+  end
+
+  monitor :vps_zombie_processes do
+    label 'Zombie processes'
+    desc 'VPS has too many zombie processes'
+    period 1*60*60
+    repeat 1*60*60 # repeat is further controlled by the called action
+    skip_acknowledged false
+
+    query do
+      ::Vps
+        .select('vpses.*, vps_os_processes.`count` AS zombie_process_count')
+        .joins(:vps_os_processes, :vps_current_status, :user)
+        .where(
+          vps_os_processes: {state: 'Z'},
+          vps_current_statuses: {status: true, is_running: true},
+          users: {object_state: ::User.object_states[:active]},
+          vpses: {object_state: ::Vps.object_states[:active]},
+        )
+    end
+
+    user do |vps|
+      vps.user
+    end
+
+    value do |vps|
+      vps.zombie_process_count
+    end
+
+    check do |vps, value|
+      value < 1000
+    end
+
+    action :alert_user_zombie_processes
   end
 end
