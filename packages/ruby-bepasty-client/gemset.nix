d49b3fca9ac3c9e6565d35ddec4e8476f2ec2cf7
Jakub Skokan <jakub.skokan@havefun.cz>
2023-05-14 10:12:18 +0200

    ruby-bepasty-client: 1.0.0 -> 1.0.1

--- packages/ruby-bepasty-client/gemset.nix
+++ packages/ruby-bepasty-client/gemset.nix
@@ -4,9 +4,9 @@
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "1r6grkl7f0gbqqh52hgpcdvzinax79whrsq8wkvvg2hvsz6gfm7s";
+      sha256 = "1n6cb4z6sa0377agf7c1pbkgcmh60sd7frkvadxg550gmpls2i8g";
       type = "gem";
     };
-    version = "1.0.0";
+    version = "1.0.1";
   };
 }
