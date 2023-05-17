2cfa5f1f50776456de1eab6acb7adb5e68edc4df
Jakub Skokan <jakub.skokan@havefun.cz>
2023-05-17 16:50:52 +0200

    internal-dns: declare address once

--- cluster/cz.vpsfree/containers/brq/int.ns1/module.nix
+++ cluster/cz.vpsfree/containers/brq/int.ns1/module.nix
@@ -1,12 +1,14 @@
 { config, ... }:
-{
+let
+  addr = "172.19.9.90";
+in {
   cluster."cz.vpsfree/containers/brq/int.ns1" = rec {
     spin = "nixos";
     swpins.channels = [ "nixos-stable" "os-staging" ];
     container.id = 21851;
-    host = { name = "ns1"; location = "int.brq"; domain = "vpsfree.cz"; target = "172.19.9.90"; };
+    host = { name = "ns1"; location = "int.brq"; domain = "vpsfree.cz"; target = addr; };
     addresses = {
-      v4 = [ { address = "172.19.9.90"; prefix = 32; } ];
+      v4 = [ { address = addr; prefix = 32; } ];
     };
     services = {
       bind = {};
--- cluster/cz.vpsfree/containers/prg/int.ns1/module.nix
+++ cluster/cz.vpsfree/containers/prg/int.ns1/module.nix
@@ -1,12 +1,14 @@
 { config, ... }:
-{
+let
+  addr = "172.16.9.90";
+in {
   cluster."cz.vpsfree/containers/prg/int.ns1" = rec {
     spin = "nixos";
     swpins.channels = [ "nixos-stable" "os-staging" ];
     container.id = 21850;
-    host = { name = "ns1"; location = "int.prg"; domain = "vpsfree.cz"; target = "172.16.9.90"; };
+    host = { name = "ns1"; location = "int.prg"; domain = "vpsfree.cz"; target = addr; };
     addresses = {
-      v4 = [ { address = "172.16.9.90"; prefix = 32; } ];
+      v4 = [ { address = addr; prefix = 32; } ];
     };
     services = {
       bind = {};
