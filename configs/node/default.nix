99c8189f2d3afd6bd44e1817eecee3584b5e596f
Jakub Skokan <jakub.skokan@havefun.cz>
2023-05-24 08:14:38 +0200

    configs/node: add alertmanager silence on halt

--- configs/node/default.nix
+++ configs/node/default.nix
@@ -3,5 +3,6 @@
   imports = [
     ./base.nix
     ./bird.nix
+    ./halt-silence.nix
   ];
 }
---
+++ configs/node/halt-silence.nix
@@ -0,0 +1,54 @@
+{ pkgs, lib, config, confMachine, ... }:
+with lib;
+let
+  alerters = [
+    "https://alerts1.prg.vpsfree.cz"
+    "https://alerts2.prg.vpsfree.cz"
+  ];
+
+  httpConfigFile = pkgs.writeText "am-http-config.yml" (builtins.toJSON {
+    basic_auth = {
+      username = "node";
+      password_file = "/var/secrets/alertmanager-http-password";
+    };
+  });
+in {
+  environment.etc = {
+    "amtool/config.yml".text = builtins.toJSON {
+      "alertmanager.url" = elemAt alerters 0;
+      "author" = confMachine.host.fqdn;
+      "require-comment" = false;
+      "http.config.file" = httpConfigFile;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [ prometheus-alertmanager ];
+
+  runit.halt.hooks = {
+    "alertmanager-silence".source = pkgs.writeScript "alertmanager-silence" ''
+      #!${pkgs.bash}/bin/bash
+
+      [ "$HALT_HOOK" != "pre-system" ] && exit 0
+
+      comment="System $HALT_ACTION
+
+      $HALT_REASON"
+      retval=
+
+      for alerter in ${concatStringsSep " " alerters} ; do
+        echo "Silencing fqdn=${confMachine.host.fqdn} on $alerter"
+        ${pkgs.prometheus-alertmanager}/bin/amtool silence add \
+          --alertmanager.url=$alerter \
+          --timeout=10s \
+          --author=$USER@${confMachine.host.fqdn} \
+          --comment="$comment" \
+          --duration=20m \
+          fqdn=${confMachine.host.fqdn}
+        retval=$?
+        [ $retval == 0 ] && exit 0
+      done
+
+      exit $retval
+    '';
+  };
+}
