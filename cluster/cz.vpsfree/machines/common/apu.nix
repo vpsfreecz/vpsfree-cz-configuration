781170c8779c9e24e8d8c98fa95c2c1aa8f3de3a
Jakub Skokan <jakub.skokan@havefun.cz>
2023-06-06 12:02:44 +0200

    apus: remove boot.loader.grub.version

--- cluster/cz.vpsfree/machines/common/apu.nix
+++ cluster/cz.vpsfree/machines/common/apu.nix
@@ -36,7 +36,6 @@ in {
   boot = {
     loader.grub = {
       enable = true;
-      version = 2;
       device = "/dev/sda";
       extraConfig = "serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1; terminal_input serial; terminal_output serial";
     };
