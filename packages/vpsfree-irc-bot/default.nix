df36988851ebdcaec669ab092f4c7793872a5d67
Jakub Skokan <jakub.skokan@havefun.cz>
2023-09-20 18:33:29 +0200

    vpsfree-irc-bot: update to 45f9d46c

--- packages/vpsfree-irc-bot/default.nix
+++ packages/vpsfree-irc-bot/default.nix
@@ -3,8 +3,8 @@ let
   vpsfbot = fetchFromGitHub {
     owner = "vpsfreecz";
     repo = "vpsfree-irc-bot";
-    rev = "3ea6a7571b6e55dd283dd860e38507a57c2c61df";
-    sha256 = "sha256-GAYVOugienvqwIhokrC4fJGFLgVfA67kmNVtp7Vrbow=";
+    rev = "45f9d46cb12b62e5acb449ca538815b70b260978";
+    sha256 = "sha256-SUKi3/wX4i0wwbWYMxcgtLkruFVS7sBI2IcU0FyQlNU=";
   };
 in bundlerEnv {
   name = "vpsfree-irc-bot";
