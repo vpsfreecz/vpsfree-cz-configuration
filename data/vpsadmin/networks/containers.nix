4e5064a6678be58d5dd3a84943f865839c1403af
Jakub Skokan <jakub.skokan@havefun.cz>
2024-02-17 11:37:49 +0100

    data: regenerate using latest confctl

--- data/vpsadmin/networks/containers.nix
+++ data/vpsadmin/networks/containers.nix
@@ -1,85 +1,114 @@
 {
-  ipv4 = [{
-    address = "172.16.10.0";
-    prefix = 24;
-  } {
-    address = "172.16.128.0";
-    prefix = 19;
-  } {
-    address = "172.16.4.0";
-    prefix = 23;
-  } {
-    address = "172.16.8.0";
-    prefix = 24;
-  } {
-    address = "172.16.9.0";
-    prefix = 24;
-  } {
-    address = "172.18.0.0";
-    prefix = 16;
-  } {
-    address = "172.19.128.0";
-    prefix = 19;
-  } {
-    address = "172.19.9.0";
-    prefix = 24;
-  } {
-    address = "185.8.164.0";
-    prefix = 25;
-  } {
-    address = "185.8.165.0";
-    prefix = 24;
-  } {
-    address = "185.8.166.0";
-    prefix = 24;
-  } {
-    address = "37.205.10.0";
-    prefix = 24;
-  } {
-    address = "37.205.11.0";
-    prefix = 24;
-  } {
-    address = "37.205.12.0";
-    prefix = 23;
-  } {
-    address = "37.205.14.0";
-    prefix = 24;
-  } {
-    address = "37.205.15.0";
-    prefix = 24;
-  } {
-    address = "37.205.8.0";
-    prefix = 23;
-  } {
-    address = "77.93.223.0";
-    prefix = 24;
-  } {
-    address = "83.167.228.0";
-    prefix = 25;
-  }];
-  ipv6 = [{
-    address = "2a01:430:17::";
-    prefix = 48;
-  } {
-    address = "2a03:3b40:100::";
-    prefix = 48;
-  } {
-    address = "2a03:3b40:101::";
-    prefix = 48;
-  } {
-    address = "2a03:3b40:110::";
-    prefix = 44;
-  } {
-    address = "2a03:3b40:200::";
-    prefix = 39;
-  } {
-    address = "2a03:3b40:20::";
-    prefix = 44;
-  } {
-    address = "2a03:3b40:3::";
-    prefix = 64;
-  } {
-    address = "2a03:3b40:fe::";
-    prefix = 48;
-  }];
+  "ipv4" = [
+    {
+      address = "83.167.228.0";
+      prefix = 25;
+    }
+    {
+      address = "37.205.8.0";
+      prefix = 23;
+    }
+    {
+      address = "37.205.10.0";
+      prefix = 24;
+    }
+    {
+      address = "185.8.165.0";
+      prefix = 24;
+    }
+    {
+      address = "172.16.8.0";
+      prefix = 24;
+    }
+    {
+      address = "172.16.9.0";
+      prefix = 24;
+    }
+    {
+      address = "37.205.11.0";
+      prefix = 24;
+    }
+    {
+      address = "185.8.166.0";
+      prefix = 24;
+    }
+    {
+      address = "172.19.9.0";
+      prefix = 24;
+    }
+    {
+      address = "185.8.164.0";
+      prefix = 25;
+    }
+    {
+      address = "172.16.10.0";
+      prefix = 24;
+    }
+    {
+      address = "37.205.12.0";
+      prefix = 23;
+    }
+    {
+      address = "37.205.14.0";
+      prefix = 24;
+    }
+    {
+      address = "172.16.4.0";
+      prefix = 23;
+    }
+    {
+      address = "172.16.128.0";
+      prefix = 19;
+    }
+    {
+      address = "172.18.0.0";
+      prefix = 16;
+    }
+    {
+      address = "77.93.223.0";
+      prefix = 24;
+    }
+    {
+      address = "172.19.128.0";
+      prefix = 19;
+    }
+    {
+      address = "37.205.15.0";
+      prefix = 24;
+    }
+  ];
+  "ipv6" = [
+    {
+      address = "2a01:430:17::";
+      prefix = 48;
+    }
+    {
+      address = "2a03:3b40:100::";
+      prefix = 48;
+    }
+    {
+      address = "2a03:3b40:3::";
+      prefix = 64;
+    }
+    {
+      address = "2a03:3b40:fe::";
+      prefix = 48;
+    }
+    {
+      address = "2a03:3b40:20::";
+      prefix = 44;
+    }
+    {
+      address = "2a03:3b40:101::";
+      prefix = 48;
+    }
+    {
+      address = "2a03:3b40:110::";
+      prefix = 44;
+    }
+    {
+      address = "2a03:3b40:200::";
+      prefix = 39;
+    }
+  ];
 }
