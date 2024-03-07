92a3e091668e0fda067b12a6978aaa2b5f30b1dc
Jakub Skokan <jakub.skokan@havefun.cz>
2024-03-07 15:43:51 +0100

    cluster: update root password on nodes

--- cluster/cz.vpsfree/nodes/common/all.nix
+++ cluster/cz.vpsfree/nodes/common/all.nix
@@ -27,7 +27,7 @@ in
     ../../vpsadmin/common/settings.nix
   ];
 
-  users.users.root.initialHashedPassword = "$6$X/q70eX.dr$svzVWUFXbcOwEtPtURVVy0n80evQMXxI4fU7ICBG5xXftWSuZh4G4zSQ8FF9mgICLfwxzFTffFcXluhn0xazH.";
+  users.users.root.initialHashedPassword = "$y$j9T$WXTPc7ms74FZcj6c7HDOO/$bAOvRUTPg8ClT5RX2pqHWezXXJM82khUZVmwxaumbpD";
 
   boot.kernelModules = [
     "ipmi_si"
