c8e3a97e39c71e49d32f26a898493c4265e010cb
Jakub Skokan <jakub.skokan@havefun.cz>
2024-05-07 13:40:54 +0200

    aitherdev: add interface for netboot server development

--- cluster/cz.vpsfree/machines/aitherdev/config.nix
+++ cluster/cz.vpsfree/machines/aitherdev/config.nix
@@ -61,6 +61,12 @@ in {
   networking.interfaces.br0.ipv4.addresses = [
     { address = "172.16.106.40"; prefixLength = 24; }
   ];
+
+  # Network for PXE development
+  networking.interfaces.enp8s0.ipv4.addresses = [
+    { address = "192.168.100.10"; prefixLength = 24; }
+  ];
+
   networking.defaultGateway = "172.16.106.1";
   networking.nameservers = internalDnsAddresses ++ [ "172.16.106.1" ];
 
