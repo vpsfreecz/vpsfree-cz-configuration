b224ea6494741a0173de94ab5767f6d68bd60fef
Jakub Skokan <jakub.skokan@havefun.cz>
2024-06-09 09:41:01 +0200

    Set security.acme.defaults.server = null

--- profiles/ct.nix
+++ profiles/ct.nix
@@ -15,5 +15,11 @@
   security.acme = {
     acceptTerms = true;
     defaults.email = "podpora@vpsfree.cz";
+
+    # NixOS >=24.05 set the default to a LE URL, which had the side effect
+    # of changing hash that identifies LE accounts. While it's not an issue
+    # for us, let's not create new accounts unnecessarily.
+    # See: https://github.com/NixOS/nixpkgs/pull/317257
+    defaults.server = null;
   };
 }
