193f2353931d19b8b814f630791d08fd2e93adf4
Jakub Skokan <jakub.skokan@havefun.cz>
2024-06-17 14:45:48 +0200

    vpsadmin-config: start backup transfers at 01:22
    node24.prg currently crashes at 01:15, which is not ideal as some
    transfers or snapshot deletions may not be saved. The previous
    cron-based scheduler may have contributed to the crashes. If the new
    ruby-based scheduler won't help, at least the backups won't start until
    later.

--- configs/vpsadmin/api/dataset_plans.rb
+++ configs/vpsadmin/api/dataset_plans.rb
@@ -2,6 +2,6 @@ VpsAdmin::API::DatasetPlans.register do
   plan :daily_backup, label: 'Daily backup',
         desc: 'Snapshot every day at 01:00 and backup on backuper.prg' do |dip|
     group_snapshot dip, '00', '01', '*', '*', '*'
-    backup dip, '15', '01', '*', '*', '*'
+    backup dip, '22', '01', '*', '*', '*'
   end
 end
