1432ac31d8de98afb8364fc7dc170a2e1edde8aa
Jakub Skokan <jakub.skokan@havefun.cz>
2024-07-25 16:04:52 +0200

    configs/goresheat-proxy: add bootstrap to index file

--- configs/goresheat-proxy.nix
+++ configs/goresheat-proxy.nix
@@ -32,13 +32,17 @@ let
     <!DOCTYPE html>
     <html>
       <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>goresheat: live system usage monitor</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
       </head>
       <body>
         <h1>goresheat: live system usage monitor</h1>
         <ul>
           ${concatMapStringsSep "\n" (m: ''<li><a href="/${m.metaConfig.host.fqdn}/">${m.metaConfig.host.fqdn}</a></li>'') allNodes}
         </ul>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
       </body>
     </html>
   '';
