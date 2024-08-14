3d13cf64550a22daa83c6126bb98176957759fa2
Jakub Skokan <jakub.skokan@havefun.cz>
2024-08-14 17:50:37 +0200

    node23.prg: split tank into two pools, tank and dozer

--- cluster/cz.vpsfree/nodes/prg/node23/config.nix
+++ cluster/cz.vpsfree/nodes/prg/node23/config.nix
@@ -10,25 +10,65 @@
   boot.zfs.pools = {
     tank = {
       install = true;
-      wipe = [ "sda" "sdb" "sdc" "sdd" "sde" "sdf" "sdg" "sdh" "sdi" "sdj" "sdk" "sdl" "sdm" "sdn" ];
+      wipe = [ "sda" "sdb" "sdc" "sdd" "sde" "sdf" "sdg" ];
       layout = [
         { type = "raidz"; devices = [ "sda" "sdb" "sdc" "sdd" "sde" "sdf" "sdg" ]; }
+      ];
+      properties = {
+        ashift = "12";
+        "feature@block_cloning" = "disabled";
+      };
+      datasets = {
+        "reservation".properties = {
+          refreservation = "400G";
+        };
+      };
+    };
+
+    dozer = {
+      install = true;
+      wipe = [ "sdh" "sdi" "sdj" "sdk" "sdl" "sdm" "sdn" ];
+      layout = [
         { type = "raidz"; devices = [ "sdh" "sdi" "sdj" "sdk" "sdl" "sdm" "sdn" ]; }
       ];
       properties = {
         ashift = "12";
+        "feature@block_cloning" = "disabled";
       };
+
+      # Configure datasets and scrub as they would be in common/tank.nix
       datasets = {
-        "reservation".properties = {
-          refreservation = "800G";
+        "/".properties = {
+          compression = "on";
+          dnodesize = "legacy";
+          recordsize = "128k";
+          xattr = "sa";
         };
+        "ct".properties = {
+          acltype = "posixacl";
+        };
+        "reservation".properties = {
+          refreservation = lib.mkDefault "400G";
+          canmount = "off";
+        };
+      };
+
+      scrub = {
+        enable = true;
+        startIntervals = [ "0 4 1-7 * *" ];
+        startCommand = ''[ "$(LC_ALL=C date '+\%a')" = "Sun" ] && scrubctl start dozer'';
       };
     };
   };
 
   osctl.pools.tank = {
-    parallelStart = 20;
-    parallelStop = 40;
+    parallelStart = 10;
+    parallelStop = 20;
+  };
+
+  osctl.pools.dozer = {
+    parallelStart = 10;
+    parallelStop = 20;
   };
 
   osctld.settings.cpu_scheduler.enable = true;
