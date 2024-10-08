cf06bd52217341bcabaf6e02fa6bdd523a0c06ca
Jakub Skokan <jakub.skokan@havefun.cz>
2024-10-08 22:33:40 +0200

    health-checks: check individual vpsAdmin API servers with curl

--- health-checks/vpsadmin/api.nix
+++ health-checks/vpsadmin/api.nix
@@ -12,4 +12,16 @@
       { property = "ActiveState"; value = "active"; }
     ];
   };
+
+  machineCommands = builtins.genList (i:
+    let
+      port = toString (9292 + i);
+    in {
+      description = "Check vpsAdmin API server #${toString i} at :${port}";
+      command = [ "curl" "--fail" "http://{addresses.primary.address}:${port}" ];
+      standardOutput.include = [
+        "HaveAPI"
+        "</html>"
+      ];
+    }) 8;
 }
