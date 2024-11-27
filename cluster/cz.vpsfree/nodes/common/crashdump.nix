340986ecc4a50b68654068245785b9c02d22e659
Jakub Skokan <jakub.skokan@havefun.cz>
2024-11-27 13:49:46 +0100

    nodes/common: rework crashdump to upload data to backuper over NFS

---
+++ cluster/cz.vpsfree/nodes/common/crashdump.nix
@@ -0,0 +1,84 @@
+{ config, pkgs, lib, confLib, confMachine, ... }:
+let
+  inherit (lib) concatStringsSep imap1 mapAttrsToList mkIf;
+
+  backuper2Prg = confLib.findMetaConfig {
+    cluster = config.cluster;
+    name = "cz.vpsfree/nodes/prg/backuper2";
+  };
+
+  networking = confMachine.osNode.networking;
+
+  renameNetif = mac: newName: ''
+    oldName=$(ip -o link | grep "${mac}" | awk -F': ' '{print $2}')
+
+    if [ -n "$oldName" ]; then
+      echo "  $oldName -> ${newName}"
+      ip link set dev "$oldName" name "${newName}"
+    else
+      echo "  interface with ${mac} not found"
+    fi
+
+    oldName=
+  '';
+
+  setupNetif = name: addresses: ''
+    ${concatStringsSep "\n" (map (addr: "ip -4 addr add ${addr.address}/${toString addr.prefix} dev ${name}") addresses.v4)}
+    ip link set ${name} up
+  '';
+in mkIf (confMachine.osNode.networking.bird.enable && confMachine.osNode.networking.bird.routingProtocol == "bgp") {
+  boot.initrd.kernelModules = [
+    "lockd"
+    "netfs"
+    "nfsv4"
+    "sunrpc"
+  ];
+
+  boot.initrd.network = {
+    # Within the crash system, we're using 10G interfaces and so the DHCP
+    # that configures 1G interfaces can and should be skipped.
+    enableSetupInCrashDump = false;
+
+    customSetupCommands = ''
+      if grep -q this_is_a_crash_kernel /proc/cmdline ; then
+        echo "Renaming network interfaces"
+        ${concatStringsSep "\n" (mapAttrsToList (name: mac: renameNetif mac name) networking.interfaces.names)}
+
+        echo "Configuring interfaces"
+        ${concatStringsSep "\n" (mapAttrsToList (name: addresses: setupNetif name addresses) networking.interfaces.addresses)}
+
+        echo "Adding default route"
+        ${concatStringsSep "\n" (imap1 (i: n: "ip -4 route add default via ${n.address} metric ${toString (100 + i)}") networking.bird.bgpNeighbours.v4)}
+      fi
+    '';
+  };
+
+  boot.initrd.extraUtilsCommands = ''
+    copy_bin_and_libs ${pkgs.nfs-utils}/bin/mount.nfs
+  '';
+
+  boot.crashDump = {
+    enable = true;
+    commands = ''
+      date=$(date +%Y%m%dT%H%M%S)
+      server="${backuper2Prg.addresses.primary.address}:/storage/vpsfree.cz/crashdump"
+      mountpoint="/mnt/nfs"
+      target="$mountpoint/${confMachine.name}/$date"
+
+      echo "Mounting NFS"
+      mkdir -p "$mountpoint"
+      mount.nfs -o vers=4 "$server" "$mountpoint" || fail "Unable to mount NFS share"
+
+      echo "Target dir $target"
+      mkdir -p "$target"
+
+      cpuCount=$(nproc)
+
+      echo "Dumping core file using $cpuCount threads"
+      LD_PRELOAD=$LD_LIBRARY_PATH/libgcc_s.so.1  makedumpfile -c -d 16 --num-threads $cpuCount /proc/vmcore "$target/dumpfile"
+
+      echo "Rebooting"
+      reboot -f
+    '';
+  };
+}
--- cluster/cz.vpsfree/nodes/common/netboot.nix
+++ cluster/cz.vpsfree/nodes/common/netboot.nix
@@ -1,10 +1,12 @@
-{ config, lib, pkgs, confLib, confMachine, ... }:
-let
-  apuPrg = confLib.findMetaConfig {
-    cluster = config.cluster;
-    name = "cz.vpsfree/machines/prg/apu";
-  };
-in {
+{ config, lib, pkgs, ... }:
+{
+  imports = [
+    # While crash dump is not limited to netbooted machines, in practice, all nodes
+    # are netbooted and other systems do not use boot.initrd.network, which is
+    # required to upload the crash dump.
+    ./crashdump.nix
+  ];
+
   boot.initrd.kernelModules = [
     "igb" "ixgbe" "tg3"
   ];
@@ -14,18 +16,7 @@ in {
     useDHCP = true;
   };
 
-  # While crash dump is not limited to netbooted machines, in practice, all nodes
-  # are netbooted and other systems do not use boot.initrd.network, which is
-  # required to upload the crash dump.
   boot.consoleLogLevel = 8;
-  # boot.crashDump = {
-  #   enable = true;
-  #   execAfterDump = ''
-  #     date=$(date +%Y%m%dT%H%M%S)
-  #     tftp -l /dmesg -r /${confMachine.host.fqdn}-dmesg-$date -p ${apuPrg.addresses.primary.address} || exit 1
-  #     reboot -f
-  #   '';
-  # };
 
   boot.postBootCommands = ''
     chmod 0600 /var/secrets/ssh_host_*_key
