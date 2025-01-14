63fa88944c9132ccefe55215a88d3e94f1d712b5
Jakub Skokan <jakub.skokan@havefun.cz>
2025-01-14 11:25:56 +0100

    clusterconf/monitor: relax PacketLoss rule to 5min duration

--- modules/clusterconf/monitor/rules/nodes.nix
+++ modules/clusterconf/monitor/rules/nodes.nix
@@ -975,7 +975,7 @@
       {
         alert = "PacketLoss";
         expr = ''rate(node_network_transmit_drop_total{device=~"(teng.+)|(oneg.+)"}[5m]) > 0 or rate(node_network_receive_drop_total{device=~"(teng.+)|(oneg.+)"}[5m]) > 0'';
-        for = "2m";
+        for = "5m";
         labels = {
           alertclass = "packetloss";
           severity = "warning";
