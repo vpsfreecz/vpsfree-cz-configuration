733235d31b1122683e3b3e130dfbb4b6970d61a9
Jakub Skokan <jakub.skokan@havefun.cz>
2025-03-29 20:24:36 +0100

    Add tools/gc-build-machine.sh

---
+++ tools/gc-build-machine.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Removing build machine generations"
+confctl generation rotate --no-gc -lr -t build
+
+echo "Removing generations of netbooted machines"
+confctl generation rotate --no-gc -lr -t pxe-primary
+
+echo "Collecting garbage"
+confctl garbage-collect -t build
