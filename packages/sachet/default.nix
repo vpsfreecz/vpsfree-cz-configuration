141b95f9d28eb4947cc10e8d3afc1d1fb8ba5dde
Jakub Skokan <jakub.skokan@havefun.cz>
2025-05-27 21:25:35 +0200

    sachet: buildGoPackage -> buildGoModule

--- packages/sachet/default.nix
+++ packages/sachet/default.nix
@@ -1,19 +1,19 @@
-{ buildGoPackage, fetchFromGitHub, lib }:
+{ buildGoModule, fetchFromGitHub, lib }:
 let
   owner = "vpsfreecz";
   repo = "sachet";
   rev = "670ac3c7dfe7aa3152525a12959abe0066d578be";
-in buildGoPackage rec {
+in buildGoModule rec {
   name = "sachet-${version}";
   # version = lib.substring 0 7 rev;
   version = "vpsfree-0.3.2";
-
-  goPackagePath = "github.com/messagebird/${repo}";
 
   src = fetchFromGitHub {
     inherit owner repo rev;
     sha256 = "sha256-BOxdIhJySYywXhOdOzXIvpdugQlIjcpJSBrOcFRmrl8=";
   };
+
+  vendorHash = null;
 
   tags = [ "release" ];
 }
