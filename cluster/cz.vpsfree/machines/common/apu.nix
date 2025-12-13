ed94f9fe6345773c63d7ba8c7ecf98785fa53e2d
Jakub Skokan <jakub.skokan@havefun.cz>
2025-12-13 11:44:07 +0100

    apu: restart modemNet every 4h

--- cluster/cz.vpsfree/machines/common/apu.nix
+++ cluster/cz.vpsfree/machines/common/apu.nix
@@ -124,7 +124,7 @@ in
       Type = "simple";
       ExecStart = "${modemNetBringUp}/bin/modem-network-bring-up";
       Restart = "always";
-      RuntimeMaxSec = "12h";
+      RuntimeMaxSec = "4h";
     };
     bindsTo = [ "sys-subsystem-net-devices-lte0.device" ];
     after = [ "sys-subsystem-net-devices-lte0.device" ];
