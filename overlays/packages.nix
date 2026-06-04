0aa4fd1bd59e42cc6304a4593eadae14a2d35c83
Jakub Skokan <jakub.skokan@havefun.cz>
2026-06-04 22:43:03 +0200

    munin: fix FastCGI Perl wrapper
    The old nixpkgsMunin fork carried package fixes for Munin FastCGI
    scripts. After moving the FastCGI service into this configuration,
    int.munin uses stock nixpkgs 26.05 Munin, whose tainted CGI scripts do
    not trust the wrapper PERL5LIB and whose CGI wrapper path lacks
    CGI::Fast.
    
    Restore those package-side fixes locally by patching the CGI scripts to
    trust the wrapper PERL5LIB under taint mode and by prepending CGI and
    CGIFast to the generated CGI wrapper path.

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -1,9 +1,31 @@
-self: super: {
+self: super:
+let
+  muninCgiPerlPath =
+    with self.perlPackages;
+    makeFullPerlPath [
+      CGI
+      CGIFast
+    ];
+in
+{
   geminabox = super.callPackage ../packages/geminabox { };
 
   node-exporter-textfile-collector-scripts =
     super.callPackage ../packages/node-exporter-textfile-collector-scripts.nix
       { };
+
+  munin = super.munin.overrideAttrs (oldAttrs: {
+    patches = (oldAttrs.patches or [ ]) ++ [
+      ../packages/munin/use-lib-in-fcgi-scripts.patch
+    ];
+
+    postFixup = (oldAttrs.postFixup or "") + ''
+      for file in "$out"/www/cgi/*; do
+        substituteInPlace "$file" \
+          --replace-fail "export PERL5LIB='" "export PERL5LIB='${muninCgiPerlPath}:"
+      done
+    '';
+  });
 
   mosh-osc-colors = super.callPackage ../packages/mosh-osc-colors.nix { };
 
